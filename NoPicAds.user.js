98fb917a04ba2cbd56789d93d7d9ec59bd1f88b2
Wei Cheng Pan <legnaleurc@gmail.com>
2012-12-28 21:34:29 +0800

    Bump version.

--- NoPicAds.user.js
+++ NoPicAds.user.js
@@ -3,7 +3,7 @@
 // @namespace      FoolproofProject
 // @description    No Picture Advertisements
 // @copyright      2012+, legnaleurc (https://github.com/legnaleurc/nopicads)
-// @version        1.1.2
+// @version        2.0.0
 // @license        BSD
 // @updateURL      http://userscripts.org/scripts/source/154858.meta.js
 // @downloadURL    http://userscripts.org/scripts/source/154858.user.js
