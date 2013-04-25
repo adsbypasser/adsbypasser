76c44a9e6305757f85f98ba8e4a55047fad6f35e
Wei Cheng Pan <legnaleurc@gmail.com>
2013-04-26 02:52:55 +0800

    imgserve.net
    Add new site.

--- NoPicAds.user.js
+++ NoPicAds.user.js
@@ -136,6 +136,7 @@
 // @match          http://comicalpic.net/img-*.html
 // @match          http://image.torrentjav.net/img-*.html
 // @match          http://imagedecode.com/*
+// @match          http://imgserve.net/img-*.html
 // @match          http://ligasampiona.com/img-*.html
 // @match          http://zeljeimage.com/img-*.html
 // @match          http://zonezeedimage.com/img-*.html
@@ -1065,7 +1066,7 @@
     {
       rule: [
         {
-          host: /(imagedecode|zonezeedimage|zeljeimage|ligasampiona)\.com|(comicalpic|image\.torrentjav)\.net/,
+          host: /(imagedecode|zonezeedimage|zeljeimage|ligasampiona)\.com|(comicalpic|image\.torrentjav|imgserve)\.net/,
         },
       ],
       run: function() {
