0cf6e0761356863843426a6619eef5e0ca87ae62
Wei Cheng Pan <legnaleurc@gmail.com>
2013-08-03 19:26:33 +0800

    bih.cc
    Add new site.

--- NoPicAds.user.js
+++ NoPicAds.user.js
@@ -169,6 +169,7 @@
 // ==adcrun==
 // @match          http://4ks.net/*
 // @match          http://adcrun.ch/*
+// @match          http://bih.cc/*
 // @match          http://fly2url.com/*
 // @match          http://link.tl/*
 // @match          http://mhz.me/*
@@ -180,6 +181,7 @@
 // @match          http://zpoz.net/*
 // @exclude        http://4ks.net/
 // @exclude        http://adcrun.ch/
+// @exclude        http://bih.cc/
 // @exclude        http://fly2url.com/
 // @exclude        http://link.tl/
 // @exclude        http://mhz.me/
@@ -752,7 +754,7 @@
       {
         rule: [
           {
-            host: /adcrun\.ch|(youlinking|fly2url)\.com|(4ks|zpoz)\.net|(shortit|tr5)\.in|(wwy|mhz)\.me|ssl\.gs|link\.tl/,
+            host: /adcrun\.ch|(youlinking|fly2url)\.com|(4ks|zpoz)\.net|(shortit|tr5)\.in|(wwy|mhz)\.me|ssl\.gs|link\.tl|bih\.cc/,
           },
         ],
         run: function () {
