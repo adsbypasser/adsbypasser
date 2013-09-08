ee5d27b2957beebd5011e0ab69eeab4dd93b4ca4
Ricky Chien <ricky060709@gmail.com>
2013-09-08 14:16:55 +0800

    vvcap.net
    Add new site.

---
+++ src/sites/vvcap.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @match          http://vvcap.net/db/*.htp
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  $.register({
+    rule: {
+      host: /vvcap\.net/
+    },
+    run: function () {
+      var i = $('img'); 
+      $.replaceBody(i.src);
+    }
+  });
+
+})();
+
+// vim: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
