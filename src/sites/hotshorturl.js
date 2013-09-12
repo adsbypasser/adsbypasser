40c0a3e267c2b76d6dcc5afc4132c35fb24275bd
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-13 00:24:38 +0800

    Tiny change.

--- src/sites/hotshorturl.js
+++ src/sites/hotshorturl.js
@@ -2,21 +2,18 @@
 // @match          http://hotshorturl.com/*
 // ==/UserScript==
 
-(function () {
-  'use strict';
+$.register({
+  rule: {
+    host: /hotshorturl\.com/,
+  },
+  run: function () {
+    'use strict';
 
-  $.register({
-    rule: {
-      host: /hotshorturl\.com/,
-    },
-    run: function () {
-      var frame = $('frame[scrolling=yes]');
-      $.redirect(frame.src);
-    },
-  });
+    var frame = $('frame[scrolling=yes]');
+    $.redirect(frame.src);
+  },
+});
 
-})();
-
-// vim: ts=2 sts=2 sw=2 et
+// ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
