b61f78da5c78e8f164aa118955064642ea0c36f8
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-13 00:29:22 +0800

    Tiny change.

--- src/sites/unfake.it.js
+++ src/sites/unfake.it.js
@@ -2,22 +2,19 @@
 // @match          http://unfake.it/*
 // ==/UserScript==
 
-(function () {
-  'use strict';
+$.register({
+  rule: {
+    host: /unfake\.it/,
+  },
+  run: function () {
+    'use strict';
 
-  $.register({
-    rule: {
-      host: /unfake\.it/,
-    },
-    run: function () {
-      var frame = $('frame');
-      var i = frame.src.lastIndexOf('http://');
-      $.redirect(frame.src.substr(i));
-    },
-  });
+    var frame = $('frame');
+    var i = frame.src.lastIndexOf('http://');
+    $.redirect(frame.src.substr(i));
+  },
+});
 
-})();
-
-// vim: ts=2 sts=2 sw=2 et
+// ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
