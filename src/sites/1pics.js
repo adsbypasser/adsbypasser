d4afa26e1b507387ba6767efb34ebbb8f6c73e83
Ricky Chien <ricky060709@gmail.com>
2013-09-19 12:51:00 +0800

    1pics.ru
    Add new site.

---
+++ src/sites/1pics.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://1pics.ru/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /1pics\.ru/,
+  },
+  run: function () {
+    'use strict';
+
+    var img = $('img[alt="Бесплатный фотохостинг 1Pics.Ru"]');
+    $.redirect(img.src);
+  },
+});
+
+// vim: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
