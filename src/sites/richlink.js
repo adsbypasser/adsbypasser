f1beb33b0ed4f985f08b3b873e6b1c3461584c1b
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-23 00:35:12 +0800

    richlink.com
    Closes #41 .

---
+++ src/sites/richlink.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @match          http://richlink.com/app/webscr?cmd=_click&key=*
+// ==/UserScript==
+
+
+$.register({
+  rule: {
+    host: /^richlink\.com$/,
+  },
+  run: function () {
+    'use strict';
+
+    var f = $('frameset');
+    f = f.onload.toString();
+    f = f.match(/url=([^&]+)/);
+    if (f) {
+      f = decodeURIComponent(f[1]);
+    } else {
+      f = $('frame[name=site]');
+      f = f.src;
+    }
+    $.redirect(f);
+  },
+});
+
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
