ec234655ee94ef6d2d198237bb388f2a9021beaf
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-23 21:44:47 +0800

    imagecherry.com
    Add new site.
    Closes #94.

---
+++ src/sites/imagecherry.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://imagecherry.com/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imagecherry\.com$/,
+  },
+  run: function () {
+    'use strict';
+
+    var i = $('img.pic');
+    $.replace(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
