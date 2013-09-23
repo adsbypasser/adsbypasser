1a0baeb1dedf33ad31f27fecb7049d0c5f3ac63a
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-23 23:09:05 +0800

    imageban.ru
    Add new site (closes #90).

---
+++ src/sites/imageban.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @include        /http://imageban\.ru/show/\d{4}/\d{2}/\d{2}/.+/
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imageban\.ru$/,
+  },
+  run: function () {
+    'use strict';
+
+    var i = $('#img_obj');
+    $.redirect(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
