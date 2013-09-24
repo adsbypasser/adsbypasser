d0ae79eb8057940985717c0c902cc15efa6ee962
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-25 01:39:55 +0800

    reffbux.com
    Add new site.

---
+++ src/sites/reffbux.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @match          http://reffbux.com/refflinx/view/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^reffbux\.com$/,
+  },
+  run: function () {
+    'use strict';
+
+    $.removeNodes('iframe');
+
+    var id = null, share = null;
+    $.$$('script').find(function (script) {
+      var m = script.innerHTML.match(/skip_this_ad_(\d+)_(\d+)/);
+      if (m) {
+        id = m[1];
+        share = m[2];
+        return true;
+      }
+      return false;
+    });
+    var fp = 0;
+    var location = window.location;
+
+    $.post('http://reffbux.com/refflinx/register', {
+      id: id,
+      share: share,
+      fp: 0,
+      location: location,
+      referer: '',
+    }, function (text) {
+      var m = text.match(/'([^']+)'/);
+      if (!m) {
+        throw new _.NoPicAdsError('pattern changed');
+      }
+      $.redirect(m[1]);
+    });
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
