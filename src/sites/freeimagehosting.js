874e8bf573c39fcdc83151effb5357a7bff3f08c
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-27 02:55:49 +0800

    freeimagehosting.net
    Add new site (closes #68).

---
+++ src/sites/freeimagehosting.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @include        /http://www\.freeimagehosting\.net/[\d\w]+$/
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^www\.freeimagehosting\.net$/,
+  },
+  run: function () {
+    'use strict';
+
+    var i = $('img');
+    $.redirect(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
