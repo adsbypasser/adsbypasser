75577a761d13cdf0ffeb9e558afeb5e046ccdd64
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-28 23:34:08 +0800

    ima.so
    Add new site (closes #85).

---
+++ src/sites/ima.so.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://ima.so/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^ima\.so$/,
+  },
+  run: function () {
+    'use strict';
+
+    var a = $('#image_block a');
+    $.redirect(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
