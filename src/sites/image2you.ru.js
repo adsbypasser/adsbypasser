b8158be27bf36815986284ec1b73d7554a683fe9
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-09-28 23:54:15 +0800

    image2you.ru
    Add new site (closes #88).

---
+++ src/sites/image2you.ru.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @include        /http://image2you\.ru/\d+/\d+/
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^image2you\.ru$/,
+  },
+  run: function () {
+    'use strict';
+
+    var i = $.$('div.t_tips2 div > img');
+    if (!i) {
+      $.postAndGo('', {
+        _confirm: '',
+      });
+      return;
+    }
+    $.redirect(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
