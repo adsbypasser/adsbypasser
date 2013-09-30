d21f59729d48290576c1acccb390386673eb5491
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-01 01:45:58 +0800

    imagepix.org
    Add new site (closes #107).

---
+++ src/sites/imagepix.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://imagepix.org/image/*.html
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imagepix\.org$/,
+  },
+  run: function () {
+    'use strict';
+
+    var i = $('img[border="0"]');
+    $.redirect(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
