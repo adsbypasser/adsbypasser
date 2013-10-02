117f25807e9f2d276df1217e23736d5674e2fa27
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-02 22:34:15 +0800

    imageshack.us
    Add new site (closes #114).

---
+++ src/sites/imageshack.us.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @match          http://imageshack.us/photo/*/*/*/
+// @match          http://imageshack.us/f/*/*/
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var host = /^imageshack\.us$/;
+
+  $.register({
+    rule: {
+      host: host,
+      path: /^\/photo\/.+\/(.+)\/([^\/]+)/,
+    },
+    run: function (m) {
+      $.redirect(_.T('/f/{0}/{1}/')(m.path[1], m.path[2]));
+    },
+  });
+
+  $.register({
+    rule: {
+      host: host,
+      path: /^\/f\/.+\/[^\/]+/,
+    },
+    run: function () {
+      var i = $('#fullimg');
+      $.redirect(i.src);
+    },
+  });
+
+})();
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
