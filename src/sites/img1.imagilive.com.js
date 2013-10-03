9488bbaa36a459ec00652af268810af723649656
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-04 00:13:32 +0800

    img1.imagilive.com
    Add new site (closes #125).

---
+++ src/sites/img1.imagilive.com.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @match          http://img1.imagilive.com/*/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^img1\.imagilive\.com$/,
+  },
+  run: function () {
+    'use strict';
+
+    var a = $.$('#page a.button');
+    if (a) {
+      $.redirect(a.href);
+      return;
+    }
+
+    var i = $('#page > img:not([id])');
+    $.redirect(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
