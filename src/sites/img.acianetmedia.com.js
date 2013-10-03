bdcf299cb61f47451f4e99f6c426a613066f892e
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-04 00:34:33 +0800

    img.acianetmedia.com
    Add new site (closes #127).

---
+++ src/sites/img.acianetmedia.com.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @include        /http://img\.acianetmedia\.com/(image/)?[^.]+/
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^img\.acianetmedia\.com$/,
+  },
+  run: function () {
+    'use strict';
+
+    var img = $('#full_image, #shortURL-content img');
+    $.redirect(img.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
