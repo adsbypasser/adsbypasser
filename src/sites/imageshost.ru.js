ab24d42f08cf743595b79dc61a02bad4190b6a68
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-03 23:35:08 +0800

    imageshost.ru
    Add new site (closes #115).

---
+++ src/sites/imageshost.ru.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://imageshost.ru/photo/*/id*.html
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imageshost\.ru$/,
+  },
+  run: function () {
+    'use strict';
+
+    var a = $('#bphoto a');
+    $.redirect(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
