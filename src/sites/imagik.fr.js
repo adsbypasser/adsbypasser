4b72e7b50b1e3d04ef89bc7588be266b24e23f6f
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-04 00:02:50 +0800

    imagik.fr
    Add new site (closes #124).

---
+++ src/sites/imagik.fr.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @match          http://imagik.fr/view*/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imagik\.fr$/,
+    path: /^\/view(-rl)?\/(.+)/,
+  },
+  run: function (m) {
+    'use strict';
+
+    // mimetype is text/plain
+    $.replace('/uploads/' + m.path[2]);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
