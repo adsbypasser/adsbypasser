ccae288a70c99f50287d598594d1e68b752bfd61
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-03 23:56:48 +0800

    imagezilla.net
    Add new site (closes #123).

---
+++ src/sites/imagezilla.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @match          http://imagezilla.net/show/*
+// ==/UserScript==
+
+$.register({
+  rule: {
+    host: /^imagezilla\.net$/,
+    path: /^\/show\/(.+)$/,
+  },
+  run: function (m) {
+    'use strict';
+
+    $.redirect('/images/' + m.path[1]);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
