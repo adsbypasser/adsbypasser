f627dab71de872c22fcdd7a5871dbdc2f8314206
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-14 02:56:26 +0800

    coinurl.com
    Fix nested iframe problem.

--- src/sites/coinurl.js
+++ src/sites/coinurl.js
@@ -1,19 +1,40 @@
-$.register({
-  rule: {
-    host: /^coinurl\.com|cur\.lv$/,
-  },
-  ready: function () {
-    'use strict';
+(function () {
+  'use strict';
 
-    var d = window.frames[0];
+  function deep (d) {
     d.addEventListener('DOMContentLoaded', function () {
       var a = $.$('#skip-ad', d.document);
       if (a) {
         $.openLink(a.href);
       }
     });
-  },
-});
+  }
+
+  $.register({
+    rule: {
+      host: /^coinurl\.com|cur\.lv$/,
+    },
+    ready: function () {
+      var d = window.frames[0];
+      if (d) {
+        deep(d);
+        return;
+      }
+      var o = new MutationObserver(function (mutations) {
+        o.disconnect();
+        var d = window.frames[0];
+        d.addEventListener('DOMContentLoaded', function () {
+          d = d.frames[0];
+          deep(d);
+        });
+      });
+      o.observe(document.body, {
+        childList: true,
+      });
+    },
+  });
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
