1487a8beba25bd44826c769344c95ce166609ebc
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-14 01:00:34 +0800

    link2you.ru
    Fix url match pattern.

--- src/sites/link2you.ru.js
+++ src/sites/link2you.ru.js
@@ -1,12 +1,16 @@
 $.register({
   rule: {
   	host: /^link2you\.ru$/,
-    path: /http:\/\/.+$/,
+    path: /^\/\d+\/(.+)$/,
   },
   start: function (m) {
     'use strict';
 
-    $.openLink(m.path[0]);
+    var url = m.path[1];
+    if (!url.match(/^https?:\/\//)) {
+      url = '//' + url;
+    }
+    $.openLink(url);
   },
 });
 
