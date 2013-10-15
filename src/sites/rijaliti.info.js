bccfb273723014eef22e21c7393521efe0f00f47
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-15 21:40:00 +0800

    rijaliti.info
    Add new site.

---
+++ src/sites/rijaliti.info.js
@@ -0,0 +1,13 @@
+$.register({
+  rule: 'http://rijaliti.info/*.php',
+  ready: function () {
+    'use strict';
+
+    var a = $('#main td[align="center"] a');
+    $.openLink(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
