789c8fa374d3bbaefe1a5eef36b381b6c5bafbe1
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-22 01:53:41 +0800

    imgadult.com
    Add new site (closes #131).

--- src/sites/reklama.js
+++ src/sites/reklama.js
@@ -1,15 +1,33 @@
-$.register({
-  rule: {
-    host: /^(image(decode|ontime)|(zonezeed|zelje|croft)image)\.com|(comicalpic|imgserve|gallerycloud)\.net|hotimages\.eu$/,
-    path: /^\/img-.*\.html$/,
-  },
-  ready: function () {
-    'use strict';
+(function () {
+  'use strict';
 
-    var o = $('img[class^=centred]');
-    $.openImage(o.src);
-  },
-});
+  function ready () {
+    var i = $('img[class^=centred]');
+    $.openImage(i.src);
+  }
+
+  $.register({
+    rule: {
+      host: /^(image(decode|ontime)|(zonezeed|zelje|croft)image)\.com|(comicalpic|imgserve|gallerycloud)\.net|hotimages\.eu$/,
+      path: /^\/img-.*\.html$/,
+    },
+    ready: ready,
+  });
+
+  $.register({
+    rule: 'http://www.imgadult.com/img-*.html',
+    start: function () {
+      var c = $.getCookie('user');
+      if (c) {
+        return;
+      }
+      $.setCookie('user', 'true');
+      window.location.reload();
+    },
+    ready: ready,
+  });
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
