122061afd262b923da78adcdcf6ed14fdd88b438
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-23 03:58:15 +0800

    Remove meanless log.

--- src/util/dom.js
+++ src/util/dom.js
@@ -230,7 +230,6 @@ var $;
       $.removeAllTimer();
       $.enableScrolling();
 
-      console.info(document.createElement('body'));
       document.body = document.createElement('body');
 
       var d = document.createElement('div');
