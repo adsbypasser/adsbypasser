c713d5544654b0375e7ebbf1cdb6337b510a2721
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-23 23:40:16 +0800

    goimagehost.com
    Fix infinity redirect problem.

--- src/sites/goimagehost.com.js
+++ src/sites/goimagehost.com.js
@@ -1,26 +1,36 @@
-$.register({
-  rule: {
-    host: /^goimagehost\.com$/,
-    path: /^\/xxx\/(.+)/,
-  },
-  start: function (m) {
-    'use strict';
+(function () {
+  'use strict';
 
-    $.openImage('/xxx/images/' + m.path[1]);
-  },
-});
+  var hostRule = /^goimagehost\.com$/;
 
-$.register({
-  rule: {
-    host: /^goimagehost\.com$/,
-    query: /^\?v=(.+)/,
-  },
-  start: function (m) {
-    'use strict';
+  $.register({
+    rule: {
+      host: hostRule,
+      path: /^\/xxx\/images\//,
+    },
+  });
 
-    $.openImage('/xxx/images/' + m.query[1]);
-  },
-});
+  $.register({
+    rule: {
+      host: hostRule,
+      path: /^\/xxx\/(.+)/,
+    },
+    start: function (m) {
+      $.openImage('/xxx/images/' + m.path[1]);
+    },
+  });
+
+  $.register({
+    rule: {
+      host: hostRule,
+      query: /^\?v=(.+)/,
+    },
+    start: function (m) {
+      $.openImage('/xxx/images/' + m.query[1]);
+    },
+  });
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
