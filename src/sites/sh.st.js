14cfe9d6bd4a8a4ab2a08d808ffa4d3f48f2a7cc
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-10-27 00:13:32 +0800

    sh.st
    Add new site (closes #344).

---
+++ src/sites/sh.st.js
@@ -0,0 +1,18 @@
+$.register({
+  rule: {
+    host: /^sh\.st$/,
+    path: /^\/[\d\w]+/,
+  },
+  ready: function () {
+    'use strict';
+
+    $.removeNodes('iframe');
+
+    var a = $('#skip_button');
+    $.openLink(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
