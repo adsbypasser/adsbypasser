6dd22c4783a78f029e4ad6bf9b4aab15aa6a4014
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-11-10 21:37:00 +0800

    goto.adflytutor.com
    Addn new site.

--- src/sites/adfly.js
+++ src/sites/adfly.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var hostRule = /^adf\.(ly|acb\.im|sazlina\.com)|[jq]\.gs|go\.phpnulledscripts\.com|(chathu|alien)\.apkmania\.co|ksn\.mx$/;
+  var hostRule = /^adf\.(ly|acb\.im|sazlina\.com)|[jq]\.gs|go\.phpnulledscripts\.com|(chathu|alien)\.apkmania\.co|ksn\.mx|goto\.adflytutor\.com$/;
 
   $.register({
     rule: {
