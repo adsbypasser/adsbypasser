945d5d28dd9f3a7a3e6c1a3968e412485a3779c9
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-11-10 21:40:18 +0800

    adfly.itsrinaldo.net
    Add new site.

--- src/sites/adfly.js
+++ src/sites/adfly.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var hostRule = /^adf\.(ly|acb\.im|sazlina\.com)|[jq]\.gs|go\.phpnulledscripts\.com|(chathu|alien)\.apkmania\.co|ksn\.mx|goto\.adflytutor\.com|dl\.apkpro\.net$/;
+  var hostRule = /^adf\.(ly|acb\.im|sazlina\.com)|[jq]\.gs|go\.phpnulledscripts\.com|(chathu|alien)\.apkmania\.co|ksn\.mx|goto\.adflytutor\.com|dl\.apkpro\.net|adfly\.itsrinaldo\.net$/;
 
   $.register({
     rule: {
