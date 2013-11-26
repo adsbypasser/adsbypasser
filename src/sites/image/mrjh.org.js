5375779d8f9d33b4af47581ff16cd0eb6c4d1a47
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-11-27 02:43:59 +0900

    mrjh.org
    Add new site (closes #157).

---
+++ src/sites/image/mrjh.org.js
@@ -0,0 +1,17 @@
+$.register({
+  rule: {
+    host: /^www\.mrjh\.org$/,
+    path: /^\/gallery\.php$/,
+    query: /^\?entry=(.+)$/,
+  },
+  ready: function (m) {
+    'use strict';
+
+    var url = m.query[1];
+    $.openImage('/' + url);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
