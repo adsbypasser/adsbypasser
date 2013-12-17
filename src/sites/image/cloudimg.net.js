d19feed0b789d89bf94c4051a5f3ccb3332f5fe4
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-12-18 01:15:10 +0900

    cloudimg.net
    Add new site.

---
+++ src/sites/image/cloudimg.net.js
@@ -0,0 +1,20 @@
+$.register({
+  rule: 'http://cloudimg.net/img-*.jpeg',
+  ready: function () {
+    'use strict';
+
+    var a = $.$('a[rel="lightbox"]');
+    if (!a) {
+      $.postAndGo('', {
+        browser_fingerprint: '',
+        ads: '0',
+      });
+      return;
+    }
+    $.openImage(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
