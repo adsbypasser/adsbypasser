fe829e7d3ca007de0a808c148f1f3f016cded959
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-02-05 04:42:35 +0900

    linkbucks.com
    Add a special case for url transition.

--- src/sites/link/linkbucks.js
+++ src/sites/link/linkbucks.js
@@ -1,7 +1,12 @@
 $.register({
-  rule: {
+  rule: [
+  {
     host: /^([\w]{8}|www)\.(allanalpass|a[mn]y|cash4files|deb|drstickyfingers|dyo|fapoff|filesonthe|free(an|gaysitepass)|galleries|goneviral|hornywood|link(babes|bucks|galleries|seer)|megaline|miniurls|picbucks|picturesetc|poontown|qqc|qvvo|realfiles|rqq|serious(deals|files|urls)|sexpalace|these(blogs|files|forums)|(these|those|u)galleries|tiny(bucks|links)|tnabucks|tubeviral|uber(picz|vidz)|ubucks|ultrafiles|url(beat|pulse)|whackyvidz|youfap|yyv|zff|zxxo)\.(com?|net|gs|me|tv|bz|us)/,
   },
+  {
+    host: /^warning-this-linkcode-will-cease-working-soon\.www\.linkbucksdns\.com$/,
+  },
+  ],
   ready: function () {
     'use strict';
 
