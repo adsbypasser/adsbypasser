ff7ee7f731e4559ddde0b0452e1f8ec077003ad7
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-03-01 19:05:42 +0900

    cashfly
    Add cashfly family. (closes #375)

---
+++ src/sites/link/cashfly.js
@@ -0,0 +1,16 @@
+$.register({
+  rule: {
+    host: /^(cf|ex|xt)\d\.(me|co)$/,
+  },
+  ready: function (m) {
+    'use strict';
+
+    $.removeNodes('iframe');
+    var a = $('#skip_button');
+    $.openLink(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
