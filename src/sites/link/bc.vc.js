06a26789013290f8a2fd9ec761f4d398f1ca4244
devnoname120 <devnoname120@gmail.com>
2014-03-17 12:58:18 +0100

    shrink.gs better integration in code

--- src/sites/link/bc.vc.js
+++ src/sites/link/bc.vc.js
@@ -194,7 +194,7 @@
 
   $.register({
     rule: {
-      host: /^adcrun\.ch|(fly2url|urlwiz)\.com|(zpoz|ultry)\.net|(wwy|myam)\.me|ssl\.gs|link\.tl|xip\.ir|hit\.us|shortit\.in|adbla\.us|www\.adjet\.eu$/,
+      host: /^adcrun\.ch|(fly2url|urlwiz)\.com|(zpoz|ultry)\.net|(wwy|myam)\.me|ssl\.gs|link\.tl|xip\.ir|hit\.us|shortit\.in|adbla\.us|www\.adjet\.eu|srk\.gs$/,
       path: /^\/.+/,
     },
     ready: run,
@@ -215,14 +215,6 @@
 
       knockServer(content, true);
     },
-  });
-
-  // shrink.gs
-    $.register({
-    rule: {
-      host: /^srk\.gs$/
-    },
-    ready: run,
   });
 
 })();
