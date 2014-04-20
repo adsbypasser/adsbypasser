891c2bd9ade099f7d91ce78086678aa283f10836
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-04-21 00:50:06 +0900

    postimg.org
    close #197

---
+++ src/sites/image/postimg.org.js
@@ -0,0 +1,15 @@
+$.register({
+  rule: {
+    host: /^postimg\.org$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var i = $('body > center > img');
+    $.openImage(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
