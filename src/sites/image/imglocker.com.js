65a6edc463b610e56fed934567a222ef8600566e
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-05-14 01:52:21 +0900

    imglocker.com
    Add new site.

---
+++ src/sites/image/imglocker.com.js
@@ -0,0 +1,15 @@
+$.register({
+  rule: {
+    host: /^imglocker\.com$/,
+    path: /^(\/\w+)\/(\w+\.\w+)$/,
+  },
+  start: function (m) {
+    'use strict';
+    var url = _.T('//img.imglocker.com{0}_{1}');
+    $.openImage(url(m.path[1], m.path[2]));
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
