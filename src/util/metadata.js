ef8bff564d2aa33d2dad6cc8583c9496c169c969
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-05-19 05:43:45 +0900

    Update metadata.

--- src/util/metadata.js
+++ src/util/metadata.js
@@ -4,8 +4,8 @@
 // @copyright      2012+, legnaleurc (https://github.com/legnaleurc/nopicads)
 // @version        <%= pkg.version %>
 // @license        BSD
-// @updateURL      https://userscripts.org/scripts/source/154858.meta.js
-// @downloadURL    https://userscripts.org/scripts/source/154858.user.js
+// @updateURL      https://legnaleurc.github.io/nopicads/releases/nopicads.meta.js
+// @downloadURL    https://legnaleurc.github.io/nopicads/releases/nopicads.user.js
 // @grant          unsafeWindow
 // @grant          GM_xmlhttpRequest
 // @grant          GM_addStyle
