a5c724da90f762fa3fd94a96f2158e17aa46ff67
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-05-28 11:50:28 +0800

    cur.lv
    Fix DOMParser bug.
    
    close #567

--- src/sites/link/coinurl.js
+++ src/sites/link/coinurl.js
@@ -9,48 +9,42 @@
     ready: function (m) {
       $.removeNodes('iframe');
       if (m.host[1] == null) {
-        var mainFrame = 'http://cur.lv/redirect_curlv.php?code=' + escape(document.location.pathname.substring(1));
+        var mainFrame = 'http://cur.lv/redirect_curlv.php?code=' + escape(window.location.pathname.substring(1));
       } else {
-        var mainFrame = 'http://cur.lv/redirect_curlv.php?zone=' + m.host[1] + '&name=' + escape(document.location.pathname.substring(1));
+        var mainFrame = 'http://cur.lv/redirect_curlv.php?zone=' + m.host[1] + '&name=' + escape(window.location.pathname.substring(1));
       }
       // Retrieve the main frame
-      $.get(mainFrame, {}, 
-        function(mainFrameContent) {
-          try {
-            // Convert it to HTML nodes
-            var docMainFrame = $.toDOM(mainFrameContent);
-          } catch (e) {
-            throw new _.NoPicAdsError('main frame changed');
-          }
-
-          // Regex allowing to extract the link from a subframe
-          var rExtractLink = /onclick="open_url\('([^']+)',\s*'go'\)/;
-
-          // Iterate each frame
-          var innerFrames = $.$$('frameset > frame', docMainFrame).each(
-            function(currFrame) {
-
-              // Fix the host of the frame
-              var currFrameAddr = currFrame.src.replace(location.hostname,m.host[2]);
-
-              // Get the content of the current frame
-              $.get(currFrameAddr, {},
-                function(currFrameContent) {
-
-                  // Try to find the link in the current frame
-                  var aRealLink = rExtractLink.exec(currFrameContent);
-
-                  // Could not find it? Try to find it in the next frame
-                  if (aRealLink == null || aRealLink[1] == null) {return;}
-
-                  // Otherwise redirect to the link
-                  var realLink = aRealLink[1];
-                  $.openLink(realLink);
-                }
-              );
-          });
+      $.get(mainFrame, {}, function(mainFrameContent) {
+        try {
+          // Convert it to HTML nodes
+          var docMainFrame = $.toDOM(mainFrameContent);
+        } catch (e) {
+          throw new _.NoPicAdsError('main frame changed');
         }
-      );
+
+        // Regex allowing to extract the link from a subframe
+        var rExtractLink = /onclick="open_url\('([^']+)',\s*'go'\)/;
+
+        // Iterate each frame
+        var innerFrames = $.$$('frameset > frame', docMainFrame).each(function (currFrame) {
+          // Fix the host of the frame
+          // NOTE Webkit does not strictly follow HTMLElement spec, can not use HTMLFrameElement.src
+          var currFrameAddr = window.location.origin + '/' + currFrame.getAttribute('src');
+
+          // Get the content of the current frame
+          $.get(currFrameAddr, {}, function(currFrameContent) {
+            // Try to find the link in the current frame
+            var aRealLink = rExtractLink.exec(currFrameContent);
+
+            // Could not find it? Try to find it in the next frame
+            if (aRealLink == null || aRealLink[1] == null) {return;}
+
+            // Otherwise redirect to the link
+            var realLink = aRealLink[1];
+            $.openLink(realLink);
+          });
+        });
+      });
     },
   });
 
