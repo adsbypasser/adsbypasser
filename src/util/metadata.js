7f6aa97118c84437107242ed6ce93feb11555d60
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-06-15 17:57:15 +0900

    Update resource domain name.

--- src/util/metadata.js
+++ src/util/metadata.js
@@ -16,8 +16,8 @@
 // @grant          GM_registerMenuCommand
 // @grant          GM_setValue
 // @run-at         document-start
-// @resource       alignCenter https://raw.github.com/legnaleurc/nopicads/v<%= pkg.version %>/css/align_center.css
-// @resource       scaleImage https://raw.github.com/legnaleurc/nopicads/v<%= pkg.version %>/css/scale_image.css
-// @resource       bgImage https://raw.github.com/legnaleurc/nopicads/v<%= pkg.version %>/img/imagedoc-darknoise.png
+// @resource       alignCenter https://raw.githubusercontent.com/legnaleurc/nopicads/v<%= pkg.version %>/css/align_center.css
+// @resource       scaleImage https://raw.githubusercontent.com/legnaleurc/nopicads/v<%= pkg.version %>/css/scale_image.css
+// @resource       bgImage https://raw.githubusercontent.com/legnaleurc/nopicads/v<%= pkg.version %>/img/imagedoc-darknoise.png
 // @include        http://*
 // @include        https://*
