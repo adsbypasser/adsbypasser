e1cafde31abfcaa20b7271194337602a09ccd978
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-07-07 02:06:15 +0900

    linkbucks.com
    Use AJAX to avoid hard reload.

--- src/sites/link/linkbucks.js
+++ src/sites/link/linkbucks.js
@@ -15,6 +15,67 @@
     /^warning-this-linkcode-will-cease-working-soon\.www\.linkbucksdns\.com$/,
   ];
 
+  function findToken (context) {
+    var script = $.$$('script', context).find(function (n) {
+      if (n.innerHTML.indexOf('window[\'init\' + \'Lb\' + \'js\' + \'\']') < 0) {
+        return _.nop;
+      }
+      return n.innerHTML;
+    });
+    if (!script) {
+      _.warn('pattern changed');
+      return null;
+    }
+    script = script.payload;
+
+    var m = script.match(/AdPopUrl\s*:\s*'.+\?ref=([\w\d]+)'/);
+    var token = m[1];
+    m = script.match(/=\s*(\d+);/);
+    var ak = parseInt(m[1], 10);
+    var re = /\+\s*(\d+);/g;
+    var tmp = null;
+    // get second (i.e. the real) salt
+    while((m = re.exec(script)) !== null) {
+      tmp = m[1];
+    }
+    ak += parseInt(tmp, 10);
+
+    return {
+      t: token,
+      aK: ak,
+    };
+  }
+
+  function sendRequest (token) {
+    _.info('sending token: %o', token);
+
+    var i = setInterval(function () {
+      $.get('/intermission/loadTargetUrl', token, function (text) {
+        var data = JSON.parse(text);
+
+        _.info('response: %o', data);
+
+        if (!data.Success && data.Errors[0] === 'Invalid token') {
+          // somehow this token is invalid, reload to get new one
+          // window.location.reload();
+          _.info('got invalid token');
+          clearInterval(i);
+          $.get(window.location.toString(), {}, function (text) {
+            var d = $.toDOM(text);
+            var t = findToken(d);
+            sendRequest(t);
+          });
+          return;
+        }
+        if (data.Success && !data.AdBlockSpotted && data.Url) {
+          clearInterval(i);
+          $.openLink(data.Url);
+          return;
+        }
+      });
+    }, 1000);
+  }
+
   $.register({
     rule: {
       host: hostRules,
@@ -46,53 +107,8 @@
         return;
       }
 
-      var script = $.$$('script').find(function (n) {
-        if (n.innerHTML.indexOf('window[\'init\' + \'Lb\' + \'js\' + \'\']') < 0) {
-          return _.nop;
-        }
-        return n.innerHTML;
-      });
-      if (!script) {
-        _.warn('pattern changed');
-        return;
-      }
-      script = script.payload;
-
-      var m = script.match(/AdPopUrl\s*:\s*'.+\?ref=([\w\d]+)'/);
-      var token = m[1];
-      m = script.match(/=\s*(\d+);/);
-      var ak = parseInt(m[1], 10);
-      var re = /\+\s*(\d+);/g;
-      var tmp = null;
-      // get second (i.e. the real) salt
-      while((m = re.exec(script)) !== null) {
-        tmp = m[1];
-      }
-      ak += parseInt(tmp, 10);
-
-      _.info({
-        t: token,
-        aK: ak,
-      });
-
-      var i = setInterval(function () {
-        $.get('/intermission/loadTargetUrl', {
-          t: token,
-          aK: ak,
-        }, function (text) {
-          var data = JSON.parse(text);
-          _.info(data);
-          if (!data.Success && data.Errors[0] === 'Invalid token') {
-            // somehow this token is invalid, reload to get new one
-            window.location.reload();
-            return;
-          }
-          if (data.Success && !data.AdBlockSpotted && data.Url) {
-            clearInterval(i);
-            $.openLink(data.Url);
-          }
-        });
-      }, 1000);
+      var token = findToken(document);
+      sendRequest(token);
     },
   });
 })();
