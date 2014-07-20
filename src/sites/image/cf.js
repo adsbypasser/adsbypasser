21e39a8efc2f46f3f6bf3bcf70c71f3936fa4b65
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-07-21 02:33:05 +0900

    images.woh.to
    Obsolete.
    close #589

--- src/sites/image/cf.js
+++ src/sites/image/cf.js
@@ -1,6 +1,6 @@
 $.register({
   rule: {
-    host: /^www\.x45x\.info|(imadul|mypixxx\.lonestarnaughtygirls)\.com|ghanaimages\.co|imgurban\.info|d69\.in|www\.images\.woh\.to$/,
+    host: /^www\.x45x\.info|(imadul|mypixxx\.lonestarnaughtygirls)\.com|ghanaimages\.co|imgurban\.info|d69\.in$/,
     query: /\?p[mt]=(.+)/,
   },
   start: function (m) {
