d1f3392e084097a80266cedb0eb914856be184fa
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-09-17 01:18:59 +0900

    Tidy files.

--- src/sites/file/dl-protect.com.js
+++ src/sites/file/dl-protect.com.js
@@ -1,21 +1,21 @@
 (function() {
+  'use strict';
+
   $.register({
     rule: {
       host: /^(www\.)?dl-protect\.com$/,
       path: /\/[A-Z1-9]+/,
     },
     ready: function () {
-      'use strict';
-
       // Button access to links
       var f = $.$('form[name=ccerure]');
       if (f) {
         var observer = new MutationObserver(function (mutations) {
           // Tracking number
-          var iIn = $.$('input[id=in]');
+          var iIn = $('input[id=in]');
 
           // If tracker (which contains a lot of information like plugins, java state, etc.) was calculated and filled in
-          for (var i=0; i < mutations.length; i++) {
+          for (var i = 0; i < mutations.length; i++) {
             if (mutations[i].target.value && mutations[i].attributeName === 'value') {
               observer.disconnect();
 
@@ -23,12 +23,13 @@
               // See https://gist.github.com/devnoname120/7cfaf46943c4f7eda290 to see how the tracking number is calculated
               iIn.value = "Tracking too much hurts users' privacy";
 
-              
               if (!canFastRedirect()) {
                 return;
               }
 
-              setTimeout(function() {f.submit();}, 600);
+              setTimeout(function() {
+                f.submit();
+              }, 600);
               break;
             }
           }
@@ -36,9 +37,11 @@
         });
 
         // Wait for the tracking number to be filled in
-        var iIn = $.$('input[id=in]');
+        var iIn = $('input[id=in]');
         if (iIn.value) {
-          setTimeout(function() {f.submit();}, 600);
+          setTimeout(function() {
+            f.submit();
+          }, 600);
         } else {
           observer.observe(iIn, {
             attributes: true,
@@ -57,12 +60,12 @@
       }
     },
   });
-  
+
   // If there is no captcha/password that the user must fill before proceeding
-  function canFastRedirect() {
+  function canFastRedirect () {
     // The onsubmit function is here only when there is a captcha
     // The pwd input is here only when we must input a password
-    return $.$('form[name=ccerure]').onsubmit === null && !$.$('form[name=ccerure] input[name=pwd]')
+    return !$.$('form[name=ccerure]').onsubmit && !$.$('form[name=ccerure] input[name=pwd]');
   }
 })();
 
