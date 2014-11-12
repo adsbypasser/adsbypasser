82976e0de2b44c789c2c959963e7b59f1d02d1a3
devnoname120 <devnoname120+gh@gmail.com>
2014-11-12 22:49:58 +0100

    Support for clictune.com
    Example link: http://www.clictune.com/id=393199

---
+++ src/sites/link/clictune.com.js
@@ -0,0 +1,19 @@
+$.register({
+  rule: {
+    host: /^(www\.)?clictune\.com$/,
+    path: /^\/id=\d+/,
+  },
+  ready: function () {
+    'use strict';
+
+    var linkHolder = $('#compteur');
+    var matches = linkHolder.innerHTML.match(/<a href=".*url=([^&"]+).*>/);
+    var url = decodeURIComponent(matches[1]);
+    
+    $.openLink(url);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
