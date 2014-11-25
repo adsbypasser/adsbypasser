6d674d6cbf918cb31313249dcc820888aaae6246
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-11-25 16:04:20 +0800

    linkbucks.com
    Handle encrypted URL.
    
    close #626

--- src/sites/link/linkbucks.js
+++ src/sites/link/linkbucks.js
@@ -46,6 +46,40 @@
     };
   }
 
+  // copy from Lbjs, can not retrive by unsafeWindow
+  function ConvertFromHex (str) {
+    var result = [];
+    while (str.length >= 2) {
+      result.push(String.fromCharCode(parseInt(str.substring(0, 2), 16)));
+      str = str.substring(2, str.length);
+    }
+    return result.join("");
+  }
+  // function name MATTERS, do not change this function
+  var Encode = function (str) {
+      var s = [], j = 0, x, res = '', k = arguments.callee.toString().replace(/\s+/g, "");
+      for (var i = 0; i < 256; i++) {
+        s[i] = i;
+      }
+      for (i = 0; i < 256; i++) {
+        j = (j + s[i] + k.charCodeAt(i % k.length)) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+      }
+      i = 0;
+      j = 0;
+      for (var y = 0; y < str.length; y++) {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+        res += String.fromCharCode(str.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
+      }
+      return res;
+    }
+
   function sendRequest (token) {
     _.info('sending token: %o', token);
 
@@ -85,9 +119,18 @@
       $.resetCookies();
       $.removeNodes('iframe');
 
-      if (m.path[1] !== null) {
-        $.openLinkWithReferer(m.path[1] + window.location.search);
+      if (!m.path[1]) {
+        throw _.AdsBypasserError('wrong url pattern');
       }
+      var url = m.path[1] + window.location.search;
+
+      var match = $.searchScripts(/UrlEncoded: ([^,]+)/);
+      if (match && match[1] === 'true') {
+        // encrypted url
+        url = Encode(ConvertFromHex(url));
+      }
+
+      $.openLinkWithReferer(url);
     }
   });
 
