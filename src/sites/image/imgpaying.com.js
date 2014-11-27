0183067b87e4fd73b999d7ec20c3e7a45b7f9aa0
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-11-27 23:09:42 +0900

    imgrock.net
    Add new site.

--- src/sites/image/imgpaying.com.js
+++ src/sites/image/imgpaying.com.js
@@ -1,28 +1,51 @@
-$.register({
-  rule: [
-    {
+(function () {
+  'use strict';
+
+  var pathRule = /^\/([^\/]+)\/[^\/]+\.[^\/]{3,4}$/;
+
+  $.register({
+    rule: {
       host: [
         /^((img(paying|mega))|imzdrop)\.com$/,
         /^(www\.)?imgsee\.me$/,
         /^imgclick\.net$/,
       ],
-      path: /^\/([^\/]+)\/[^\/]+\.[^\/]{3,4}$/,
+      path: pathRule,
     },
-  ],
-  ready: function () {
-    'use strict';
+    ready: function () {
+      var i = $.$('img.pic');
+      if (!i) {
+        // first stage
+        i = $('form');
+        i.submit();
+        return;
+      }
+      $.openImage(i.src);
+    },
+  });
 
-    var i = $.$('img.pic');
-    if (!i) {
-      // first stage
-      i = $('form');
-      i.submit();
-      return;
-    }
-    $.openImage(i.src);
-  },
-});
+  $.register({
+    rule: {
+      host: /^imgrock\.net$/,
+      path: pathRule,
+    },
+    ready: function (m) {
+      var i = $.$('img.pic');
+      if (!i) {
+        // first stage
+        $.openLinkByPost('', {
+          op: 'view',
+          id: m.path[1],
+          pre: 1,
+          next: 'Continue to Image...',
+        });
+        return;
+      }
+      $.openImage(i.src);
+    },
+  });
 
+})();
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
