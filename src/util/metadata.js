cc85612757ba6eeda4925ff5d2d5536d2c55758f
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-01-12 05:03:18 +0900

    Don't include resources in lite version.

--- src/util/metadata.js
+++ src/util/metadata.js
@@ -11,16 +11,20 @@
 // @icon           https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= pkg.version %>/img/logo.png
 // @grant          unsafeWindow
 // @grant          GM_xmlhttpRequest
+<% if (!lite) { %>
 // @grant          GM_addStyle
 // @grant          GM_getResourceText
 // @grant          GM_getResourceURL
+<% } %>
 // @grant          GM_getValue
 // @grant          GM_openInTab
 // @grant          GM_registerMenuCommand
 // @grant          GM_setValue
 // @run-at         document-start
+<% if (!lite) { %>
 // @resource       alignCenter https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= pkg.version %>/css/align_center.css
 // @resource       scaleImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= pkg.version %>/css/scale_image.css
 // @resource       bgImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= pkg.version %>/img/imagedoc-darknoise.png
+<% } %>
 // @include        http://*
 // @include        https://*
