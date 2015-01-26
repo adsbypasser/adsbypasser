debea4a6219ddb82084456c6f6e704fb6b4c27d0
devnoname120 <devnoname120+gh@gmail.com>
2015-01-26 21:25:31 +0100

    Support for leechbd.tk
    I could have done without the API, the direct link is also present on
    the page.

---
+++ src/sites/link/leechbd.tk.js
@@ -0,0 +1,22 @@
+$.register({
+  rule: {
+    host: /^(www\.)?leechbd\.tk$/,
+    path: /^\/Shortener\/(\w+)$/,
+  },
+  ready: function (m) {
+    'use strict';
+
+    $.get('/Shortener/API/read/get', {id: m.path[1], type: 'json'}).then(function (text) {
+      var r = JSON.parse(text);
+      if (r.success == true && r.data.full) {
+        $.openLink(r.data.full);
+      } else {
+        _.warn('API Error ' + r.error.code + ' : ' + r.error.msg);
+      }
+    });
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
