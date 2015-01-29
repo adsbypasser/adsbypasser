76f49221176caae1bb961b45f73d50b93327c9f5
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-01-29 23:03:18 +0900

    a.pomf.se
    Add new site.

---
+++ src/sites/image/a.pomf.se.js
@@ -0,0 +1,17 @@
+http:///tpzvni.htm
+$.register({
+  rule: {
+    host: /^a\.pomf\.se$/,
+    path: /^\/.+\.htm$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var a = $('body > a');
+    $.openImage(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
