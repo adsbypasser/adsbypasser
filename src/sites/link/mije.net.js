330d5fe8d1106b38de34fc3725328d3a2d7f2f91
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-04-07 01:11:35 +0900

    mije.net
    close #672

---
+++ src/sites/link/mije.net.js
@@ -0,0 +1,19 @@
+$.register({
+  rule: {
+    host: /^www\.mije\.net$/,
+    path: /^\/\w+\/(.+)$/,
+  },
+  start: function (m) {
+    'use strict';
+
+    var url = atob(m.path[1]);
+    if (!url.match(/^https?:\/\//)) {
+      return;
+    }
+    $.openLink(url);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
