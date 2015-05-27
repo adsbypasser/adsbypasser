653998db89c3f47858816055c5ee6e9c9fd0ace6
devnoname120 <devnoname120+gh@gmail.com>
2015-05-27 19:04:36 +0200

    Minor gca.sh refactoring
    repla.cr was not skipped in case of in-URL link.

--- src/sites/link/gca.sh.js
+++ src/sites/link/gca.sh.js
@@ -1,33 +1,38 @@
-$.register({
-  rule: {
-    host: /^gca\.sh$/,
-    path: /^\/adv\/\w+\/(.*)$/,
-    query: /^(.*)$/,
-    hash: /^(.*)$/,
-  },
-  start: function (m) {
-    'use strict';
+(function () {
 
-    var l = m.path[1] + m.query[1] + m.hash[1];
-    $.openLinkWithReferer(l);
-  },
-});
+  var hosts = /^gca\.sh|repla\.cr$/;
+  $.register({
+    rule: {
+      host: hosts,
+      path: /^\/adv\/\w+\/(.*)$/,
+      query: /^(.*)$/,
+      hash: /^(.*)$/,
+    },
+    start: function (m) {
+      'use strict';
 
-$.register({
-  rule: {
-    host: /^gca\.sh|repla\.cr$/,
-  },
-  ready: function () {
-    'use strict';
+      var l = m.path[1] + m.query[1] + m.hash[1];
+      $.openLinkWithReferer(l);
+    },
+  });
 
-    $.removeNodes('iframe');
+  $.register({
+    rule: {
+      host: hosts,
+    },
+    ready: function () {
+      'use strict';
 
-    var jQuery = $.window.$;
-    setTimeout(function () {
-      jQuery("#captcha-dialog").dialog("open");
-    }, 1000);
-  },
-});
+      $.removeNodes('iframe');
+
+      var jQuery = $.window.$;
+      setTimeout(function () {
+        jQuery("#captcha-dialog").dialog("open");
+      }, 1000);
+    },
+  });
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
