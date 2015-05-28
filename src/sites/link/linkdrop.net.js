573c78fd6582f5a983807b524fbfbd5e982ff467
devnoname120 <devnoname120+gh@gmail.com>
2015-05-28 16:20:54 +0200

    Support for linkdrop.net
    Close #699

---
+++ src/sites/link/linkdrop.net.js
@@ -0,0 +1,22 @@
+$.register({
+  rule: {
+    host: /^(www\.)?linkdrop\.net$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var matches = $.searchScripts(/\$\("a\.redirect"\)\.attr\("href","([^"]+)"\)\.text\("Continue"\);/);
+
+    // Most likely not on a shortening page
+    if (!matches) {
+        return;
+    }
+
+    var l = matches[1];
+    $.openLink(l);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
