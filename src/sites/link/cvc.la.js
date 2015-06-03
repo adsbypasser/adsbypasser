e5991f735683103dba7a0afbbfa035ee54d87c0a
devnoname120 <devnoname120+gh@gmail.com>
2015-06-03 12:57:30 +0200

    Support for cvc.la
    https://greasyfork.org/forum/discussion/4505/

---
+++ src/sites/link/cvc.la.js
@@ -0,0 +1,21 @@
+$.register({
+  rule: {
+    host: /^(www\.)?cvc\.la$/,
+    path: /^\/\w+$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var matches = $.searchScripts(/<input type='hidden' name='hidden' value='(\d+)'>/);
+    setTimeout(function() {
+      $.openLinkByPost(document.location.href, {
+        hidden: matches[1],
+        image: ' ',
+      });
+    }, 100);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
