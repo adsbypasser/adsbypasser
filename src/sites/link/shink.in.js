15080a1c7186c6e7022ffdd616b8f71ee03c94ad
devnoname120 <devnoname120+gh@gmail.com>
2015-06-03 18:02:47 +0200

    Support shink.in
    Close https://greasyfork.org/forum/discussion/4505/

---
+++ src/sites/link/shink.in.js
@@ -0,0 +1,17 @@
+$.register({
+  rule: {
+    host: /^(www\.)?shink\.in$/,
+    path: /^\/\w+$/,
+  },
+  ready: function () {
+    'use strict';
+
+      var f = $('#skip');
+      // Specifying a correct #id input value seems unecessary
+      f.submit();
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
