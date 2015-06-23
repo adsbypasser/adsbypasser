ce3814e26a6372dbe1bd6928a973d78ac202b9c6
devnoname120 <devnoname120+gh@gmail.com>
2015-06-23 21:26:45 +0200

    Support link.im
    Close https://greasyfork.org/forum/discussion/4795/

---
+++ src/sites/link/link.im.js
@@ -0,0 +1,20 @@
+$.register({
+  rule: {
+    host: /^(www\.)?link\.im$/,
+    path: /^\/\w+$/,
+  },
+  start: function () {
+    'use strict';
+
+    $.post(document.location.href, {
+      image: 'Continue',
+    }).then(function (text) {
+      var m = text.match(/window\.location\.replace\('([^']+)'\)/);
+      $.openLink(m[1]);
+    });
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
