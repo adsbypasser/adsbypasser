9380c6b28fdddb2eca43219d696fa253d0e8248d
devnoname120 <devnoname120+gh@gmail.com>
2015-06-23 21:12:34 +0200

    Support for victly.com
    Close #713

---
+++ src/sites/link/victly.com.js
@@ -0,0 +1,21 @@
+$.register({
+  rule: {
+    host: /^(www\.)?victly\.com$/,
+    path: /^\/\w+$/,
+  },
+  start: function () {
+    'use strict';
+
+    $.post(document.location.href, {
+      hidden: '',
+      image: 'Skip+Ads',
+    }).then(function (text) {
+      var m = text.match(/window\.location\.replace\('([^']+)'\)/);
+      $.openLink(m[1]);
+    });
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
