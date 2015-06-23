b77635741a2a79fe995da0e3926bb217202d0df8
devnoname120 <devnoname120+gh@gmail.com>
2015-06-23 22:07:01 +0200

    Support for boxcash.net
    Close https://greasyfork.org/forum/discussion/4797/
    
    Example link:
    http://boxcash.net/2DSx
    http://boxcash.net/redirect.html?bid=792&url=http://example.com

---
+++ src/sites/link/boxcash.net.js
@@ -0,0 +1,41 @@
+// Note: Captcha is verified client-side
+$.register({
+  rule: {
+    host: /^(www\.)?boxcash\.net$/,
+    path: /^\/\w+$/,
+  },
+  ready: function () {
+    'use strict';
+
+    // JSON.parse() is not used because their JSON is malformed
+    var m = $.searchScripts(/\'\/ajax_link\.php\',\{key:'(\w+)',url:'(\d+)',t:'(\d+)',r:'(\w*)'\}/);
+
+    $.post('/ajax_link.php', {
+      key: m[1],
+      url: m[2],
+      t: m[3],
+      r: m[4],
+    }).then(function (response) {
+      var l = response.match(/window(?:.top.window).location="([^"]+)"/);
+      $.openLink(l[1]);
+    });
+  },
+});
+
+$.register({
+  rule: {
+    host: /^(www\.)?boxcash\.net$/,
+    path: /^\/redirect\.html$/,
+    query: /url=(.+)$/,
+  },
+  start: function (m) {
+    'use strict';
+    
+    $.openLink(m.query[1]);
+  },
+});
+
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
