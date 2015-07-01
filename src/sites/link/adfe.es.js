279a41e86bc9cc02c886054814450eb528e10c32
devnoname120 <devnoname120+gh@gmail.com>
2015-07-01 16:21:42 +0200

    Support for adfe.es
    Close https://greasyfork.org/forum/discussion/4903/
    Close #727

---
+++ src/sites/link/adfe.es.js
@@ -0,0 +1,22 @@
+$.register({
+  rule: {
+    host: /^(www\.)?adfe\.es$/,
+    path: /^\/\w+$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var f = $('#frmvideo');
+
+    // Not at the final step?
+    if (!f.STEP4) {
+    	return;
+    }
+
+    f.submit();
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
