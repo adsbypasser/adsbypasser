4ad2a43899f87556def4cd7283270a56b7add9f4
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-08-23 04:22:40 +0900

    nimplus.com
    close #762

--- src/sites/image/reklama.js
+++ src/sites/image/reklama.js
@@ -34,7 +34,7 @@
           // starts with www
           /^www\.(imagefolks|img(blow|lemon))\.com$/,
           // else
-          /^(picstwist|ericsony|wpc8|uplimg|xxx\.pornprimehd|lexiit|thumbnailus)\.com$/,
+          /^(picstwist|ericsony|wpc8|uplimg|xxx\.pornprimehd|lexiit|thumbnailus|nimplus)\.com$/,
           /^((i|hentai)\.)?imgslip\.com$/,
           /^(i|xxx)\.hentaiyoutube\.com$/,
           /^(go|er)imge\.com$/,
