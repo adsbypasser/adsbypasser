488329f9822609c6006396f7a0efdb596e2e1b60
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-09-22 23:36:20 +0900

    daily-img.com
    Fix url pattern.

--- src/sites/image/dailyss.net.js
+++ src/sites/image/dailyss.net.js
@@ -2,7 +2,7 @@ $.register({
   rule: {
     host: [
       /^dailyss\.net$/,
-      /^daily-img\.com$/,
+      /^(www\.)daily-img\.com$/,
       /^img-365\.com$/,
     ],
     path: /^\/image\/.+$/,
