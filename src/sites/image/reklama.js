e5324d09cc046f25bcccb28ab28e678bb929fad8
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-11-03 05:56:40 +0900

    imgbinbou.com
    close #834

--- src/sites/image/reklama.js
+++ src/sites/image/reklama.js
@@ -28,7 +28,7 @@
           // ends with image
           /^(zonezeed|zelje|croft|myhot|bok|hostur|greasy)image\.com$/,
           // starts with img
-          /^img(next|savvy|\.spicyzilla|twyti|xyz|devil|tzar|ban|pu|beer|wet|tornado|kicks|-pay|nimz)\.com$/,
+          /^img(next|savvy|\.spicyzilla|twyti|xyz|devil|tzar|ban|pu|beer|wet|tornado|kicks|-pay|nimz|binbou)\.com$/,
           // starts with img-
           /^img-(zone|planet)\.com$/,
           // starts with www
