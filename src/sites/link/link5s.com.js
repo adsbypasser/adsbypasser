48154dbb6a5cccc6e02421fd7d13e35d17b41a3e
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-11-08 07:04:52 +0900

    link5s.com
    close #838

---
+++ src/sites/link/link5s.com.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  function sendRequest (opts) {
+    return $.post('/ajax/r.php', opts).then(function (data) {
+      _.info(data);
+      if (data.length <= 1) {
+        return sendRequest(opts);
+      }
+      var a = $.toDOM(data);
+      a = $('a', a);
+      _.info(a);
+      _.info(a.href);
+      return a.href;
+    });
+  }
+
+  $.register({
+    rule: {
+      host: /^link5s\.com$/,
+      path: /^\/([^\/]+)$/,
+    },
+    ready: function (m) {
+      'use strict';
+
+      // disable page ajax
+      $.window.$ = null;
+
+      var i = $('#iframeID');
+      var opts = {
+        page: m.path[1],
+        advID: i.dataset.cmp,
+        u: i.dataset.u,
+      };
+      $.removeNodes('iframe');
+
+      sendRequest(opts).then(function (url) {
+        $.openLink(url);
+      });
+    },
+  });
+
+})();
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
