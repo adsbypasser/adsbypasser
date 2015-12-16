ef7d523f1319e34e22a2e6457eda1202f138a2a9
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-12-16 01:27:29 -0500

    streamingfrench.net
    close #863

---
+++ src/sites/link/streamingfrench.net.js
@@ -0,0 +1,17 @@
+$.register({
+  rule: {
+    host: /^streamingfrench\.net$/,
+    path: /^\/$/,
+    query: /^\?xb=(.+)$/,
+  },
+  start: function (m) {
+    'use strict';
+
+    var url = decodeURIComponent(m.query[1]);
+    $.openLink(url);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
