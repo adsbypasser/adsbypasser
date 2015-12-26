899bdc3c2ff26bd07e96c1ce15e22cc109d74cd7
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-12-27 04:31:31 +0900

    linksas.us
    Assuming all uses recaptcha.
    
    close #873

--- src/sites/link/linksas.us.js
+++ src/sites/link/linksas.us.js
@@ -6,22 +6,43 @@ $.register({
   ready: function (m) {
     'use strict';
 
-    var token = $.getCookie('XSRF-TOKEN');
-    var payload = JSON.stringify({
-      ipAddress: $.generateRandomIP(),
-      country: '',
-      recaptcha: '',
-    });
-
-    $.post('/go' + m.path[1], payload, {
-      'Content-Type': 'application/json',
-      'X-XSRF-TOKEN': token,
+    _.try(1000, function () {
+      var recaptcha = $('#g-recaptcha-response');
+      if (!recaptcha) {
+        // no recaptcha, unlikely to happen
+        return null;
+      }
+      if (!recaptcha.value) {
+        // not ready yet
+        return _.none;
+      }
+      return recaptcha.value;
+    }).then(function (recaptcha) {
+      // get random ip info
+      var url = _.T('http://ipinfo.io/{0}/json')($.generateRandomIP());
+      return $.get(url).then(function (ipinfo) {
+        ipinfo = _.parseJSON(ipinfo);
+        return {
+          codeAds: 1,
+          country: ipinfo.country,
+          ipAddress: ipinfo.ip,
+          recaptcha: recaptcha,
+        };
+      });
+    }).then(function (payload) {
+      // finally send the request
+      var token = $.getCookie('XSRF-TOKEN');
+      return $.post('/go' + m.path[1], payload, {
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': token,
+      });
     }).then(function (data) {
-      data = JSON.parse(data);
+      data = _.parseJSON(data);
       $.openLink(data.message);
     }).catch(function (e) {
       _.warn(e);
     });
+
   },
 });
 
