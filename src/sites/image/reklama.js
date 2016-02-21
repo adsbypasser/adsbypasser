190000a77b7a2505ead03d3b8e18ab58bfa3c36c
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-02-22 04:38:55 +0900

    myimg.info
    close #925

--- src/sites/image/reklama.js
+++ src/sites/image/reklama.js
@@ -63,7 +63,7 @@
           /^(hotimages|55888)\.eu$/,
           /^img(cloud|mag)\.co$/,
           /^pixup\.us$/,
-          /^(bulkimg|photo-up)\.info$/,
+          /^(bulkimg|photo-up|myimg)\.info$/,
           /^img\.yt$/,
           /^vava\.in$/,
           /^(pixxx|picspornfree|imgload)\.me$/,
