2f430808ab4a712b1e264b18c45d84d0b8621e6a
Ercan <ercanalayli@yandex.com>
2016-02-23 21:28:16 +0000

    ads-img.info
    added ads-img.info image hosting website

--- src/sites/image/reklama.js
+++ src/sites/image/reklama.js
@@ -63,7 +63,7 @@
           /^(hotimages|55888)\.eu$/,
           /^img(cloud|mag)\.co$/,
           /^pixup\.us$/,
-          /^(bulkimg|photo-up|myimg|pop-img)\.info$/,
+          /^(bulkimg|photo-up|myimg|pop-img|ads-img)\.info$/,
           /^img\.yt$/,
           /^vava\.in$/,
           /^(pixxx|picspornfree|imgload)\.me$/,
