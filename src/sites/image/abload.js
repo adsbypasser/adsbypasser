c227e059cc747d61561abe4e79a09860e3ab15f4
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-03-13 23:04:33 +0900

    Refactor rules.

--- src/sites/image/abload.js
+++ src/sites/image/abload.js
@@ -8,7 +8,7 @@
 
   $.register({
     rule: {
-      host: [/^www\.imagepearl\.com$/, /^imagepearl\.com$/],
+      host: /^(www\.)?imagepearl\.com$/,
       path: /^\/verify\/(.+)$/,
     },
     start: function (m) {
@@ -22,11 +22,13 @@
     rule: [
       'http://*.abload.de/image.php?img=*',
       'http://www.imageup.ru/*/*/*.html',
-      'http://itmages.ru/image/view/*/*',  // different layout same handler
-      'http://imagepearl.com/view/*',  // different layout same handler
-      'http://imagepearl.com/image/*',  // different layout same handler
-      'http://www.imagepearl.com/view/*',  // different layout same handler
-      'http://www.imagepearl.com/image/*',  // different layout same handler
+      // different layout same handler
+      'http://itmages.ru/image/view/*/*',
+      // different layout same handler
+      {
+        host: /^(www\.)?imagepearl\.com$/,
+        path: /^\/(view|image)\//,
+      },
     ],
     ready: run,
   });
