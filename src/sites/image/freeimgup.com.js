341a45711c0bbf40cd573b081cd23c8120d584e1
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-04-24 00:10:18 +0900

    freeimgup.com
    close #991

---
+++ src/sites/image/freeimgup.com.js
@@ -0,0 +1,16 @@
+$.register({
+  rule: {
+    host: /^freeimgup\.com$/,
+    path: /^\/xxx\//,
+  },
+  ready: function () {
+    'use strict';
+
+    var img = $('#mainimage');
+    $.openImage(img.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
