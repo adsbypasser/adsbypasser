cc360501fbacd6f11bcb3b32a56426ed5b312151
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-04-24 00:24:04 +0900

    ysf.pl
    close #986

--- src/sites/link/ysf.pl.js
+++ src/sites/link/ysf.pl.js
@@ -11,6 +11,22 @@ $.register({
   },
 });
 
+$.register({
+  rule: {
+    host: /^ysf\.pl$/,
+    path: /^\/2\/(.+)$/,
+  },
+  start: function (m) {
+    'use strict';
+
+    var url = m.path[1].match(/.{2}/g).map(function (h) {
+      return String.fromCharCode(parseInt(h, 16));
+    }).join('');
+
+    $.openLink(url);
+  },
+});
+
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
