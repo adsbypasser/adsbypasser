be453e92d70a5a4765a1cbcac50de8f3bdbcefd5
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-05-02 05:15:31 +0900

    picstream.tv
    close #1021

---
+++ src/sites/image/picstream.tv.js
@@ -0,0 +1,16 @@
+$.register({
+  rule: {
+    host: /^picstream\.tv$/,
+    path: /^\/.*\/.*\.html$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var img = $('#view1 > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)');
+    $.openImage(img.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
