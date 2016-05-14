ba80e79cceadc17da633b4b1a5f8668b5be9ad9a
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-05-14 20:07:56 +0900

    imgsay.com
    Fix wrong MIME type.

--- src/sites/image/imgfantasy.js
+++ src/sites/image/imgfantasy.js
@@ -1,26 +1,41 @@
-$.register({
-  rule: {
-    host: [
-      /^img(fantasy|leech|\.pornleech|smile|say|nemo|sense)\.com$/,
-      /^(imagedomino|lovechix)\.com$/,
-      /^imageporn\.eu$/,
-      /^0img\.net$/,
-      /^daily-img\.com$/,
-      /^picangel\.in$/,
-    ],
-    query: /^\?[pv]=/,
-  },
-  ready: function () {
-    'use strict';
+(function () {
+  'use strict';
 
+  function helper (doReplace) {
     if ($.window.confirmAge) {
       $.window.confirmAge(1);
       return;
     }
     var i = $('#container-home img[onclick]');
-    $.openImage(i.src);
-  },
-});
+    $.openImage(i.src, {
+      replace: doReplace,
+    });
+  }
+
+  $.register({
+    rule: {
+      host: [
+        /^img(fantasy|leech|\.pornleech|smile|nemo|sense)\.com$/,
+        /^(imagedomino|lovechix)\.com$/,
+        /^imageporn\.eu$/,
+        /^0img\.net$/,
+        /^daily-img\.com$/,
+        /^picangel\.in$/,
+      ],
+      query: /^\?[pv]=/,
+    },
+    ready: _.P(helper, false),
+  });
+
+  $.register({
+    rule: {
+      host: /^imgsay\.com$/,
+      query: /^\?[pv]=/,
+    },
+    ready: _.P(helper, true),
+  });
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
