ee18b7008626150a4e59f3650a07dbc4d009ae18
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-05-14 22:07:40 +0900

    pixsense.net
    close #1034

---
+++ src/sites/image/pixsense.net.js
@@ -0,0 +1,28 @@
+$.register({
+  rule: {
+    host: /^www\.pixsense\.net$/,
+    path: /^\/site\/v\/(\d+)$/,
+  },
+  start: function (m) {
+    'use strict';
+
+    $.openLink('/site/viewFinalImage/' + m.path[1]);
+  },
+});
+
+$.register({
+  rule: {
+    host: /^www\.pixsense\.net$/,
+    path: /^\/site\/viewFinalImage\/\d+$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var i = $('#myUniqueImg');
+    $.openLink(i.src);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
