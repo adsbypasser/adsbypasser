36f7680005686195eed65e89c62f8a178430e52e
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-07-05 07:52:19 +0900

    Move code.

--- src/sites/image/dailyss.net.js
+++ src/sites/image/dailyss.net.js
@@ -17,6 +17,20 @@ $.register({
   },
 });
 
+$.register({
+  rule: {
+    host: /^xxx\.porn0day.\.com$/,
+    path: /^\/image\/.+$/,
+  },
+  ready: function () {
+    'use strict';
+
+    // the URL in img is a thumbnail
+    var i = $('link[rel^=image_src]');
+    $.openImage(i.href);
+  },
+});
+
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
--- src/sites/image/imagescream.com.js
+++ src/sites/image/imagescream.com.js
@@ -53,19 +53,6 @@ $.register({
   },
 });
 
-$.register({
-  rule: {
-    host: /^xxx\.porn0day.\.com$/,
-    path: /^\/image\/[A-Za-z0-9]{3,}/,
-  },
-  ready: function () {
-    'use strict';
-
-    var i = $('link[rel^=image_src]');
-    $.openImage(i.href);
-  },
-});
-
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
