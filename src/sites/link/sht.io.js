009c23bc81834ed0b0a7a58cac3059c7da089837
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-07-05 07:16:44 +0900

    Add comment.

--- src/sites/link/sht.io.js
+++ src/sites/link/sht.io.js
@@ -7,6 +7,7 @@ $.register({
     'use strict';
 
     var url = atob(m.path[1]);
+    // the salt is like: XXX{sht-io}url{sht-io}{sht-io}standard
     url = url.match(/\{sht-io\}(.+)\{sht-io\}.*\{sht-io\}/);
     $.openLink(url[1]);
   },
