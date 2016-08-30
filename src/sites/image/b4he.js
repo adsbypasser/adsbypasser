f5555102ad5cc3f3fb67633f60f1f0dd7f623f34
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-08-31 02:40:52 +0900

    imagep2p.com
    close #1155

--- src/sites/image/b4he.js
+++ src/sites/image/b4he.js
@@ -23,6 +23,18 @@ $.register({
   },
 });
 
+$.register({
+  rule: {
+    host: /^imagep2p\.com$/,
+    query: /^\?v=([^&]+)/,
+  },
+  start: function (m) {
+    'use strict';
+
+    $.openImage('/images/' + m.query[1] + '.jpeg');
+  },
+});
+
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
 // kate: space-indent on; indent-width 2;
