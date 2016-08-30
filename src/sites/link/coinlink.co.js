cb91ce2bab56c38623feb5320761947aa4215205
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-08-31 02:24:35 +0900

    coinlink.co
    close #1152

---
+++ src/sites/link/coinlink.co.js
@@ -0,0 +1,16 @@
+$.register({
+  rule: {
+    host: /^coinlink\.co$/,
+    path: /^\/i\//,
+  },
+  ready: function (m) {
+    'use strict';
+
+    var a = $('#btn-main');
+    $.openLink(a.href);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
