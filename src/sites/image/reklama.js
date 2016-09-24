b6aece8344f8a30d2841acb0f576de6e01f54a0f
Liwei Chen <liwei@itg.com.cn>
2016-09-24 09:42:02 +0800

    add imagerar.com

--- src/sites/image/reklama.js
+++ src/sites/image/reklama.js
@@ -272,7 +272,34 @@
     ready: defaultAction,
   });
 
+  $.register({
+    rule: {
+      host: /^imagerar\.com$/,
+      path: /^\/img-/
+    },
+    ready: function () {
+      $.openLink(linkid, {replace: true});
+    },
+  });
+
+  $.register({
+    rule: {
+      host: /^imagerar\.com$/,
+      path: /^\/img2-/
+    },
+    ready: function () {
+      var i = $('img[title="Click to view full size"]');
+      if (i) {
+        $.openImage(i.src, {replace: false});
+        return;
+      }
+    },
+  });
+
+
+
 })();
+
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
