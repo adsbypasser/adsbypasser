eb6ad03f1ecb1839a45e9cf87fb00cc948da7633
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-11-28 05:33:03 +0900

    viid.me
    close #1235

--- src/sites/link/sh.st.js
+++ src/sites/link/sh.st.js
@@ -29,7 +29,7 @@
     }, 1000);
   }
 
-  var hostRules = /^sh\.st|(dh10thbvu|u2ks|jnw0|qaafa)\.com|digg\.to$/;
+  var hostRules = /^sh\.st|(dh10thbvu|u2ks|jnw0|qaafa)\.com|digg\.to|viid\.me$/;
 
   $.register({
     rule: {
