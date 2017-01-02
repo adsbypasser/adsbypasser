c34468eb5dd54de807e38951dd168ee8811713ba
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-01-03 00:09:03 +0900

    filesbucks.com
    close #1265

--- src/sites/link/adlink.guru.js
+++ src/sites/link/adlink.guru.js
@@ -1,31 +1,64 @@
-$.register({
-  rule: {
-    host: [
-      /^adlink\.guru$/,
-      /^cypt\.ga$/,
-    ],
-  },
-  ready: function () {
-    'use strict';
+(function () {
+  'use strict';
 
-    var f = $('#go-link');
-    var args = {};
-    $.$$('input', f).each(function (v) {
-      args[v.name] = v.value;
+  $.register({
+    rule: {
+      host: [
+        /^adlink\.guru$/,
+        /^cypt\.ga$/,
+        /^filesbucks\.com$/,
+      ],
+    },
+    ready: function () {
+      firstStage().then(function (page) {
+        return secondStage(page);
+      }).then(function (url) {
+        $.openLink(url);
+      }).catch(function (e) {
+        _.warn(e);
+      });
+    },
+  });
+
+  function firstStage () {
+    return _.D(function (resolve, reject) {
+      var f = $.$('#link-view');
+      if (!f) {
+        resolve(document);
+        return;
+      }
+
+      var args = extractArgument(f);
+      var url = f.getAttribute('action');
+      var p = $.post(url, args).then(function (data) {
+        return $.toDOM(data);
+      });
+      resolve(p);
     });
+  }
 
-    $.post(f.getAttribute('action'), args).then(function (data) {
+  function secondStage (page) {
+    var f = $('#go-link', page);
+    var args = extractArgument(f);
+    var url = f.getAttribute('action');
+    return $.post(url, args).then(function (data) {
       data = JSON.parse(data);
       if (data && data.url) {
-        $.openLink(data.url);
-      } else {
-        _.warn('wrong data');
+        return data.url;
       }
-    }).catch(function (e) {
-      _.warn(e);
+      throw new _.AdsBypasserError('wrong data');
     });
-  },
-});
+  }
+
+  function extractArgument (form) {
+    var args = {};
+    $.$$('input', form).each(function (v) {
+      args[v.name] = v.value;
+    });
+    return args;
+  }
+
+})();
 
 // ex: ts=2 sts=2 sw=2 et
 // sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
