f39196733a338ef4712a834fa6a5eb57fffaa048
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-01-03 04:16:55 +0900

    gsurl.me
    close #1272

---
+++ src/sites/link/gsurl.me.js
@@ -0,0 +1,19 @@
+$.register({
+  rule: {
+    host: /^gsurl\.me$/,
+  },
+  ready: function () {
+    'use strict';
+
+    $.removeNodes('#container');
+
+    var a = $('#link');
+    _.wait(5000).then(function () {
+      $.openLink(a.href);
+    });
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
