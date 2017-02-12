c61885036d839f9490323de6e664dfc68c473631
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-02-12 18:50:56 +0900

    indexmovie.xyz
    close #1294

---
+++ src/sites/link/indexmovie.me.js
@@ -0,0 +1,15 @@
+$.register({
+  rule: {
+    host: /^indexmovie\.me$/,
+    path: /^\/([^\/]+)$/,
+  },
+  start: function (m) {
+    'use strict';
+
+    $.openLink('/get/' + m.path[1]);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
