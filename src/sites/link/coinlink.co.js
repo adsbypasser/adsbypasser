e8fdf13e9f31029e6482cbd489444b5822ab28c3
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-03-01 06:25:26 +0900

    coinlink.co
    close #1351

--- src/sites/link/coinlink.co.js
+++ src/sites/link/coinlink.co.js
@@ -6,7 +6,8 @@ $.register({
   ready: function (m) {
     'use strict';
 
-    var a = $('a#btn-main, a.btn.btn-block.btn-warning');
+    // this site frequently changes its style, just keep all pattern here
+    var a = $('a#btn-main, a.btn.btn-block.btn-warning, a.btn.btn-block.btn-success');
     $.openLink(a.href);
   },
 });
