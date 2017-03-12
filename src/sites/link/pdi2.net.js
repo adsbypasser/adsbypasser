02a98a19adb8a77d432953d79784ad8c22f08b75
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-03-12 18:51:39 +0900

    pdi2.net
    close #1372

---
+++ src/sites/link/pdi2.net.js
@@ -0,0 +1,16 @@
+$.register({
+  rule: {
+    host: /^pdi2\.net$/,
+  },
+  ready: function () {
+    'use strict';
+
+    var s = $.searchScripts(/top\.location = '([^']+)'/);
+    s = s[1];
+    $.openLink(s);
+  },
+});
+
+// ex: ts=2 sts=2 sw=2 et
+// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
+// kate: space-indent on; indent-width 2;
