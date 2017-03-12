fbaaa69319db821e3b0b8fea1658c274223ce9fe
DandyClubs <dandyclubs@gmail.com>
2017-03-12 21:38:35 +0900

    hashe.in add
    hashe.in add

--- src/sites/link/adlink.guru.js
+++ src/sites/link/adlink.guru.js
@@ -11,6 +11,7 @@
         /^(payurl|urlst)\.me$/,
         /^url\.ht$/,
         /^urle\.co$/,
+        /^hashe\.in$/,
         /^www\.worldhack\.net$/,
         /^123link\.top$/,
         /^pir\.im$/,
