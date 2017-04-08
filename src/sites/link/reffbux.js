5c9b985c42230948a6896e0b92d088587c2415eb
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 08:15:10 +0900

    remove modelines

--- src/sites/link/reffbux.js
+++ src/sites/link/reffbux.js
@@ -25,7 +25,3 @@ $.register({
     });
   },
 });
-
-// ex: ts=2 sts=2 sw=2 et
-// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
-// kate: space-indent on; indent-width 2;
--- src/sites/link/urlv2.com.js
+++ src/sites/link/urlv2.com.js
@@ -14,12 +14,8 @@ $.register({
 
     var m = $.searchScripts(/jeton=([\w]+)/);
     var l = 'http://urlv2.com/algo.php?action=passer&px=0&so=1&jeton=' + m[1];
-    
+
     // Necessary because of server-side checks
     window.setTimeout(function() {$.openLink(l)}, 5000);
   },
 });
-
-// ex: ts=2 sts=2 sw=2 et
-// sublime: tab_size 2; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
-// kate: space-indent on; indent-width 2;
