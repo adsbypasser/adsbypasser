3deaa0aac75bae6106143d3a6eb37ac87c76402d
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 18:02:07 +0900

    refactor link part n

--- src/sites/link/nmac.to.js
+++ src/sites/link/nmac.to.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^nmac\.to$/,
     path: /^\/download\/(.+)/,
   },
-  start: function (m) {
-    'use strict';
-
-    var url = atob(m.path[1]);
-    $.openLink(url);
+  async start (m) {
+    const url = atob(m.path[1]);
+    await $.openLink(url);
   },
 });
