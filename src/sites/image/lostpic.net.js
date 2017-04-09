434febcafba0248ff9097df388966fa92f90ca5f
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 14:40:09 +0900

    refactor image part l

--- src/sites/image/lostpic.net.js
+++ src/sites/image/lostpic.net.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^lostpic\.net$/,
     query: /^\?photo=\d+$/,
   },
-  ready: function () {
-    'use strict';
-
-    var i = $('img.notinline.circle');
-    $.openImage(i.src);
+  async ready () {
+    const i = $('img.notinline.circle');
+    await $.openImage(i.src);
   },
 });
