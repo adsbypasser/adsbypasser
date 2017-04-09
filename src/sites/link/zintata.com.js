149e8182c06b1ee701afe28edabf410a7958ef0d
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 18:25:42 +0900

    refactor link part z

--- src/sites/link/zintata.com.js
+++ src/sites/link/zintata.com.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^www\.zintata\.com$/,
     path: /^\/link\/$/,
   },
-  ready: function () {
-    'use strict';
-
-    var a = $('#one > center:nth-child(3) > a:nth-child(1)');
-    $.openLink(a.href);
+  async ready () {
+    const a = $('#one > center:nth-child(3) > a:nth-child(1)');
+    await $.openLink(a.href);
   },
 });
