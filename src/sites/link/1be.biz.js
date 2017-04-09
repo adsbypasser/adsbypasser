8ae1a78e8b5df1f17aaebc45c326554eb932955e
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 15:21:12 +0900

    refactor link part 1

--- src/sites/link/1be.biz.js
+++ src/sites/link/1be.biz.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^(www\.)?1be\.biz$/,
     path: /^\/s\.php$/,
     query: /^\?(.+)/,
   },
-  start: function (m) {
-    'use strict';
-
-    $.openLink(m.query[1]);
+  async start (m) {
+    await $.openLink(m.query[1]);
   },
 });
