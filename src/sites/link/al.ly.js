6d5cfd50100d076d84f0a5293c93cd0ecc657982
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-17 06:00:09 +0900

    al.ly
    close #1379

--- src/sites/link/al.ly.js
+++ src/sites/link/al.ly.js
@@ -8,13 +8,29 @@ $.register({
   ready: function () {
     'use strict';
 
-    $.removeNodes('iframe, #CashSlideDiv, #ct_catfish');
+    // first stage
+    var i = $.$('body > section > iframe');
+    if (i) {
+      // the site will detect iframe being removed or not
+      i.src = 'about:blank';
 
-    var a = $('#modal-shadow');
-    a.style.display = 'block';
-    a = $('#modal-alert');
-    a.style.left = 0;
-    a.style.top = 80;
-    a.style.display = 'block';
+      // wait a reasonable time to avoid AdsBlock detection
+      _.wait(3000).then(function () {
+        var a = $('a.redirect');
+        a.click();
+      });
+
+      return;
+    }
+
+    // second stage
+    i = $.searchScripts(/"href","([^"]+)"\)\.remove/);
+    if (!i) {
+      _.warn('site changed');
+      return;
+    }
+    i = i[1];
+
+    $.openLink(i);
   },
 });
