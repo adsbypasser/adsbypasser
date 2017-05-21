6502d870be57d7a0061d0c60b6a1640c197f10eb
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-05-21 21:00:05 +0900

    fix syntax error

--- src/sites/image/imageheli.com.js
+++ src/sites/image/imageheli.com.js
@@ -1,11 +1,10 @@
 $.register({
-    rule: {
-      host: [
-        /^imageheli\.com$/,
-        /^imgtube\.net$/,
-        /^pixliv\.com$/,
-      ],
-    },
+  rule: {
+    host: [
+      /^imageheli\.com$/,
+      /^imgtube\.net$/,
+      /^pixliv\.com$/,
+    ],
     path: /^\/img-([a-zA-Z0-9\-]+)\..+$/,
   },
   ready: function () {
