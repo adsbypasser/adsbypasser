83d31bd41105d389debb5effca63673208dc5cc3
callmenemo491 <callmenemo491@users.noreply.github.com>
2017-05-21 20:00:44 +0900

    Update sylnk.net.js (#1495)
    * Update sylnk.net.js
    
    refact
    
    * Update sylnk.net.js
    
    reverting the "(www\.)?" back to "www\.", as per past pull request,  making "www" optional, will be done case-by-case not generically.

--- src/sites/link/sylnk.net.js
+++ src/sites/link/sylnk.net.js
@@ -155,11 +155,9 @@ $.register({
 $.register({
   rule: {
     host: [
-      /^designinghomey\.com$/,
+      /^(designinghomey|ani-share|sinopsisfilmku)\.com$/,
       /^motonews\.club$/,
       /^(autofans|landscapenature)\.pw$/,
-      /^ani-share\.com$/,
-      /^sinopsisfilmku\.com$/,
       /^(sidespace|erogedownload)\.net$/,
     ],
     query: /get=([^&]+)/,
