42ba5d489593fcaeebb3f398e5b22d5262b8c1cd
admin <admin@sector14.co.uk>
2017-05-28 15:38:29 +0100

    Fixed: pronpic.org
    tested by using following URLs:
    http://pronpic.org/1443124800_1735456/
    http://pronpic.org/1443124800_1735457/

--- src/sites/image/pronpic.org.js
+++ src/sites/image/pronpic.org.js
@@ -5,8 +5,10 @@ $.register({
   ready: function () {
     'use strict';
 
+    var urlBaseImg = $('table.new_table2:nth-child(1) img.link');
+    var baseUrl = urlBaseImg.src.split('th_')[0];
     var img = $('table.new_table2:nth-child(2) img.link');
-    var url = img.src.replace('th_', '');
+    var url = baseUrl + img.src.split('th_')[1];
     $.openImage(url);
   },
 });
