ba2bce81f085fd72a89b86db8cc9134d5558ed19
callmenemo491 <callmenemo491@users.noreply.github.com>
2017-10-26 04:34:18 +0900

    Update clictune.com.js (#1746)
    close #1749

--- src/sites/link/clictune.com.js
+++ src/sites/link/clictune.com.js
@@ -1,11 +1,11 @@
 _.register({
   rule: {
     host: /^(www\.)?clictune\.com$/,
-    path: /^\/id=\d+/,
+    path: /^\/[^/]+$/,
   },
   async ready () {
     $.remove('iframe');
-    const matches = $.searchFromScripts(/<a href="http:\/\/(?:www.)?clictune\.com\/redirect\.php\?url=([^&]+)&/);
+    const matches = $.searchFromScripts(/<a href="http:\/\/(?:www.)?clictune\.com\/link\/redirect\/?url=([^&]+)&/);
     const url = decodeURIComponent(matches[1]);
     await $.openLink(url);
   },
