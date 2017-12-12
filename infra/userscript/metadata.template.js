f6dc3623d903de0183148875be8772de280cacaa
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-12-12 17:16:20 -0600

    re-organize files

--- infra/userscript/metadata.template.js
+++ infra/userscript/metadata.template.js
@@ -8,7 +8,7 @@
 // @supportURL     https://github.com/adsbypasser/adsbypasser/issues
 // @updateURL      https://adsbypasser.github.io/releases/adsbypasser.<%= buildName %>.meta.js
 // @downloadURL    https://adsbypasser.github.io/releases/adsbypasser.<%= buildName %>.user.js
-// @icon           https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/img/logo.png
+// @icon           https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/resources/img/logo.png
 // @grant          GM_deleteValue
 <% if (supportImage) { %>
 // @grant          GM_getResourceURL
@@ -28,9 +28,9 @@
 // @grant          GM.xmlHttpRequest
 // @grant          unsafeWindow
 <% if (supportImage) { %>
-// @resource       alignCenter https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/css/align_center.css
-// @resource       scaleImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/css/scale_image.css
-// @resource       bgImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/img/imagedoc-darknoise.png
+// @resource       alignCenter https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/resources/css/align_center.css
+// @resource       scaleImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/resources/css/scale_image.css
+// @resource       bgImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v<%= version %>/resources/img/imagedoc-darknoise.png
 <% } %>
 // @run-at         document-start
 // @include        http://*
