5eb9670834c7e5e736e7cfe902e2e4dae997ee65
nbr63120 <35303793+nbr63120@users.noreply.github.com>
2018-01-14 19:38:35 +0900

    imgtaxi/imgadult fix
    old profiles are should disabled

--- src/sites/image/imgdrive.com.js
+++ src/sites/image/imgdrive.com.js
@@ -2,6 +2,8 @@
   rule: [
     'http://imgdrive.net/img-*',
     'http://imgwallet.com/img-*',
+    'http://imgtaxi.com/img-*',
+    'http://imgadult.com/img-*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
