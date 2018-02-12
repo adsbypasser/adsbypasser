81f2959536a2f2e4f69120b25e97ab93ce6f5672
Putu Ardi Dharmayasa <36402967+yasawibu@users.noreply.github.com>
2018-02-12 23:19:02 +0700

    Update otoviral.racing.js

--- src/sites/link/otoviral.racing.js
+++ src/sites/link/otoviral.racing.js
@@ -1,9 +1,9 @@
 _.register({
   rule: {
     host: [
-    /^otoviral\.racing$/,
-    /^www\.lifesurance\.info$/,
-    ]
+      /^otoviral\.racing$/,
+      /^www\.lifesurance\.info$/,
+    ],
   },
   async ready () {
     const a = $.searchFromScripts(/var a='([^']+)'/);
