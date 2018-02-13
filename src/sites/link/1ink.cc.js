ac0d2bff264243b27dadea6d5434d43ffd2672f8
Putu Ardi Dharmayasa <36402967+yasawibu@users.noreply.github.com>
2018-02-13 21:29:00 +0700

    Add 1ink.cc
    Solve this issue #1896

---
+++ src/sites/link/1ink.cc.js
@@ -0,0 +1,13 @@
+_.register({
+  rule: {
+    host: /^1ink\.cc$/,
+    path: /^\/\w+$/,
+  },
+  async ready () {
+    let url = document.head.querySelector('[name=keywords]').content;
+    const urlCheck = url.includes('http');
+    if (urlCheck != true)
+        url = "http://" + url;
+    await $.openLink(url);
+  },
+});
