0d41cc3d9db601194ce1fea5b55e86ecf148acae
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-04-08 23:25:14 +0900

    code linting

--- src/sites/link/ur.ly.js
+++ src/sites/link/ur.ly.js
@@ -1,9 +1,12 @@
 _.register({
   rule: {
-    host: [/^ur\.ly$/, /^urly\.mobi$/],
-    path: /^\/x(.+)/
+    host: [
+      /^ur\.ly$/,
+      /^urly\.mobi$/,
+    ],
+    path: /^\/x(.+)/,
   },
-  async ready() {
+  async ready () {
     const path = window.location.href.replace('/x', '/goii/');
     await $.openLink(path);
   }
