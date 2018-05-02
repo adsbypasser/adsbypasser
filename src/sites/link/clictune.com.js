be7e09ecd9cb7c246f9195bdfc1e562b3c13654f
Call Me Nemo <callmenemo491@users.noreply.github.com>
2018-05-02 19:59:41 +0900

    Update clictune.com.js
    #2069

--- src/sites/link/clictune.com.js
+++ src/sites/link/clictune.com.js
@@ -1,11 +1,11 @@
 _.register({
   rule: {
-    host: /^(www\.)?clictune\.com$/,
+    host: /^(www\.)?mylink\.zone$/,
     path: /^\/[^/]+$/,
   },
   async ready () {
     $.remove('iframe');
-    const matches = $.searchFromScripts(/<a href="http:\/\/(?:www\.)?clictune\.com\/link\/redirect\/\?url=([^&]+)&/);
+    const matches = $.searchFromScripts(/<a href="http:\/\/(?:www\.)?mylink\.zone\/link\/redirect\/\?url=([^&]+)&/);
     const url = decodeURIComponent(matches[1]);
     await $.openLink(url);
   },
