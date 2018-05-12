916537703eb3d5114d580ecb81f97bd68ce0017d
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-05-13 04:45:02 +0900

    remove domain
    adshort.im
    adshort.pro
    
    redirect to another domain

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -121,7 +121,7 @@
         /^linclik\.com$/,
         /^zeiz\.me$/,
         /^adbull\.me$/,
-        /^adshort\.(in|im|pro|co)$/,
+        /^adshort\.(in|co)$/,
         /^(adshorte|adsrt)\.com$/,
         /^weefy\.me$/,
         /^bit-url\.com$/,
