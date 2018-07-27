246d11eb7c80e2a3eb6c4936eaf13bce288be49e
ch4ch4 <vic.cosmic@gmail.com>
2018-07-28 01:33:12 +0700

    Add files via upload

--- src/sites/link/davinsurance.js
+++ src/sites/link/davinsurance.js
@@ -4,23 +4,23 @@ _.register({
     query: /r=(.+)/,
   },
   async start (m) {
-    const e = m.query[1];
+      const e = m.query[1];
 
-    function base64_decode(w) {
-    var m, b, z, k, x, q, A, y, v = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+      function base64_decode(w) {
+      let m, b, z, k, x, q, A, y, v = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
         s = 0,
         j = 0,
-        u = "",
+        u = '',
         p = [];
-    if (!w) {
-        return w
-    }
-    w += "";
-    do {
-        k = v.indexOf(w.charAt(s++)), x = v.indexOf(w.charAt(s++)), q = v.indexOf(w.charAt(s++)), A = v.indexOf(w.charAt(s++)), y = k << 18 | x << 12 | q << 6 | A, m = y >> 16 & 255, b = y >> 8 & 255, z = 255 & y, 64 == q ? p[j++] = String.fromCharCode(m) : 64 == A ? p[j++] = String.fromCharCode(m, b) : p[j++] = String.fromCharCode(m, b, z)
-    } while (s < w.length);
-    return u = p.join(""), decodeURIComponent(escape(u.replace(/\0+$/, "")))
-}
-    await $.openLink(base64_decode(e));
+      if (!w) {
+        return w;
+      }
+      w += '';
+      do {
+        k = v.indexOf(w.charAt(s++)), x = v.indexOf(w.charAt(s++)), q = v.indexOf(w.charAt(s++)), A = v.indexOf(w.charAt(s++)), y = k << 18 | x << 12 | q << 6 | A, m = y >> 16 & 255, b = y >> 8 & 255, z = 255 & y, 64 == q ? p[j++] = String.fromCharCode(m) : 64 == A ? p[j++] = String.fromCharCode(m, b) : p[j++] = String.fromCharCode(m, b, z);
+      } while (s < w.length);
+      return u = p.join(''), decodeURIComponent(escape(u.replace(/\0+$/, '')));
+      }
+      await $.openLink(base64_decode(e));
   },
 });
