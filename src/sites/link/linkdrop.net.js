eb49dfdbfb94806b94b952431c8fdaef2848865e
Version Kinda <version365@users.noreply.github.com>
2018-08-13 17:02:05 +0530

    fix for itiad.com
    close #2500

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -24,7 +24,7 @@
       host: [
         // com
         /^(dz4link|gocitlink|3rabcut|short2win)\.com$/,
-        /^(tmearn|payshorturl|urltips|shrinkearn)\.com$/,
+        /^(tmearn|payshorturl|urltips|shrinkearn|itiad)\.com$/,
         /^(earn-url|bit-url|cut-win|link-zero|cut-earn)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links)\.com$/,
@@ -85,7 +85,7 @@
     rule: {
       host: [
         // com
-        /^(cut-urls|linclik|premiumzen|shrt10|itiad|by6dk|mikymoons|man2pro|mykinggo)\.com$/,
+        /^(cut-urls|linclik|premiumzen|shrt10|by6dk|mikymoons|man2pro|mykinggo)\.com$/,
         /^short\.pastewma\.com$/,
         /^linkfly\.gaosmedia\.com$/,
         /^(adshorte|adsrt)\.com$/,
