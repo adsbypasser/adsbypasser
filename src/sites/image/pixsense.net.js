6fa2fa658ba06d47fe4c4414d65519a1d7d61c87
nbr63120 <nbr63120@gmail.com>
2018-12-05 18:26:05 +0900

    imgfile+etc new method
    new method for
    imgfile.net/[a-z0-9]{4,10} type URL

--- src/sites/image/pixsense.net.js
+++ src/sites/image/pixsense.net.js
@@ -12,3 +12,23 @@ _.register({
     await $.openLink(a.href);
   },
 });
+
+_.register({
+  rule: {
+    host: [
+      /^www\.pixsense\.net$/,
+      /^www\.imagespicy\.site$/,
+      /^www\.(imgsky|imgfile)\.net$/,
+    ],
+    path: /^\/[a-z|0-9]{4,10}$/,
+  },
+  async ready () {
+    await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__["_"].wait(1000);
+    const b = Object(_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__["$"])('body .main-content-box');
+    b.style.display = 'initial';
+    const c = Object(_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__["$"])('body .container');
+    c.style.display = 'initial';
+    const img = Object(_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__["$"])('body .big_img img');
+    await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__["$"].openImage(img.src);
+  },
+});
