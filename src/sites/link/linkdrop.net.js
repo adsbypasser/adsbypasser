715ce4020f1601c41857d0c85c0577a8e962c6b3
kyokokken <jaajaa3@gmail.com>
2019-01-08 07:48:18 +0100

    add cuee.io (#2709)
    add cuee.io

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -43,7 +43,7 @@
         /^idsly\.(com|bid)$/,
         /^(adbilty|adpop|payskip|wicr|ujv|tpx|adsrt)\.me$/,
         /^wi\.cr$/,
-        /^(oke|cuon|cuio|linktor|flylink)\.io$/,
+        /^(oke|cuon|cuio|cuee|linktor|flylink)\.io$/,
         /^(3bst|coinlink|itiurl|coshink|link5s)\.co$/,
         /^(shink|shrten|gg-l|vnurl)\.xyz$/,
         /^mlink\.club$/,
