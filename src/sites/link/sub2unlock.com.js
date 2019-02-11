e9105c3becd875ad69757221a59603fc8aac612a
Version Kinda <version365@users.noreply.github.com>
2019-02-11 15:33:04 +0530

    Create sub2unlock.com.js
    close #2741

---
+++ src/sites/link/sub2unlock.com.js
@@ -0,0 +1,21 @@
+_.register({
+  rule: {
+    host: /^sub2unlock\.com$/,
+    path: /^\/link\/get\//,
+  },
+  async ready () {
+    const su = $('a#link.unlock-step-link.getlink');
+    await $.openLink(su.href);
+  },
+});
+
+_.register({
+  rule: {
+    host: /^sub2unlock\.com$/,
+    path: /^\/[a-zA-Z0-9]+/,
+  },
+  async ready () {
+    const su = $.searchFromScripts(/'href'\, '([^']+)'/);
+    await $.openLink(su[1]);
+  },
+});
