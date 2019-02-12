a8b25b4aeb61dff6ea74660b61711a7c39fa7895
hpo14 <5903619+hpo14@users.noreply.github.com>
2019-02-12 14:08:20 +0800

    Update imgvip.net.js
    fix imgfile.net / pixsense.net

--- src/sites/image/imgvip.net.js
+++ src/sites/image/imgvip.net.js
@@ -21,12 +21,7 @@ _.register({
     path: /^\/[a-z|0-9]{4,10}$/,
   },
   async ready () {
-    await _.wait(1000);
-    const b = $('body .main-content-box');
-    b.style.display = 'initial';
-    const c = $('body .container');
-    c.style.display = 'initial';
-    const img = $('body .big_img img');
-    await $.openImage(img.src);
+    const matches = $.searchFromScripts(/document.getElementById("soDaBug").src = "([^"]+)";/);
+    await $.openImage(matches[1]);
   },
 });
