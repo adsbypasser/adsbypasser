a2b33272a51c8889e8cd9003bbcb0d27caa4a3a9
Version Kinda <version365@users.noreply.github.com>
2019-02-12 10:46:11 +0530

    gsmzone.site
    close #2780

--- src/sites/link/binbox.io.js
+++ src/sites/link/binbox.io.js
@@ -1,9 +1,15 @@
 // Binbox links
 _.register({
-  rule: {
-    host: /^(www\.)?([a-zA-Z0-9]+\.)?binbox\.io$/,
-    path: /\/o\/([a-zA-Z0-9]+)/,
-  },
+  rule: [
+    {
+     host: /^(www\.)?([a-zA-Z0-9]+\.)?binbox\.io$/,
+     path: /\/o\/([a-zA-Z0-9]+)/,
+    },
+    {
+     host: /^gsmzone\.site$/,
+     path: /\/go\/([a-zA-Z0-9]+)/,
+    },
+  ],
   async start (m) {
     const direct_link = window.atob(m.path[1]);
     await $.openLink(direct_link);
