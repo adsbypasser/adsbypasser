03b79bfaa08f7dea3988094caf3b5a7270bc3a2e
Call Me Nemo <callmenemo491@users.noreply.github.com>
2019-03-01 18:58:28 +0900

    Update openload.co.js
    close #2843

--- src/sites/file/openload.co.js
+++ src/sites/file/openload.co.js
@@ -2,7 +2,7 @@ _.register({
   rule: {
     host: [
       /^openload\.(co|pw)$/,
-      /^oload\.(stream|info|site|tv|win|download|cloud|cc|fun|club|live)$/,
+      /^oload\.(stream|info|site|tv|win|download|cloud|cc|fun|club|live|space)$/,
     ],
     path: /^\/f\/.*/,
   },
