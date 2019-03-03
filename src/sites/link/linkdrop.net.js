be5b51b2f49ffdc246c4dcf3ad761e0c8a092c73
Version Kinda <version365@users.noreply.github.com>
2019-03-03 07:28:19 +0530

    enewstalk.com
    close #2842

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -25,7 +25,7 @@
       host: [
         // com
         /^(dz4link|gocitlink|3rabcut|short2win|adsrt|shortglobal)\.com$/,
-        /^(tmearn|payshorturl|urltips|shrinkearn|itiad|cutsouf)\.com$/,
+        /^(tmearn|payshorturl|urltips|shrinkearn|itiad|cutsouf|enewstalk)\.com$/,
         /^(earn-url|bit-url|cut-win|link-zero|cut-earn|oturl|glory-link)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links|adskipme|skipurls)\.com$/,
