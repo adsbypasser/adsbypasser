811c514cb9db2798181b861d67484fe9efd6a1b9
jan.karberg <jan.karberg@quodata.de>
2019-03-06 20:23:07 +0100

    add more
    closes #2835
    closes #2852
    closes #2834

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -35,7 +35,7 @@
         /^(mykinggo|li-nkz|win4cut|khabratk|programsfre|safelinkblogger)\.com$/,
         /^(linkorlink|mrfourtech|fabsdeals|tech4utoday|urlsamo|icutlink)\.com$/,
         /^(earnmoneytalk|yourw-ay|reb7konline|enewstalk|newupdatesonline)\.com$/,
-        /^(uptoos)\.com$/,
+        /^(uptoos|bakilink|gossipcorners|jainjinvani)\.com$/,
         // net
         /^(safelinku|tinylinks|licklink|linkrex|zlshorte)\.net$/,
         /^(vnurl|vinaurl|foxurl|short2win|cashat|shrtfly)\.net$/,
