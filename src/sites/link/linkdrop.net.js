5373bcd01d2e9ad6d534ef3199b48cb0c31a08a9
jan.karberg <jan.karberg@quodata.de>
2019-03-06 20:04:37 +0100

    add cl250
    closes #2857

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -51,7 +51,7 @@
         /^(oke|cuon|cuio|cuee|cuus|cuto|cu2|linktor|flylink|uiz)\.io$/,
         /^(3bst|coinlink|itiurl|coshink|link5s|curs)\.co$/,
         /^(shink|shrten|gg-l|vnurl|bloggingdekh|sh11)\.xyz$/,
-        /^mlink\.club$/,
+        /^(mlink|cl250)\.club$/,
         /^(igram|gram)\.im$/,
         /^(clk|cll)\.(press|ink|sh|icu)$/,
         /^short\.pe$/,
