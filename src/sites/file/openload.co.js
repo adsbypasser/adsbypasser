a7a978b22af59150a0e142fbb417c422913e6c19
Version Kinda <version365@users.noreply.github.com>
2019-03-22 10:26:22 +0530

    Update openload.co.js
    close #2931

--- src/sites/file/openload.co.js
+++ src/sites/file/openload.co.js
@@ -3,7 +3,7 @@ _.register({
     host: [
       /^openload\.(co|pw)$/,
       /^oload\.(stream|info|site|tv|win|download|cloud|cc|fun|club|live|space)$/,
-      /^oladblock\.services$/,
+      /^oladblock\.(services|xyz)$/,
     ],
     path: /^\/f\/.*/,
   },
