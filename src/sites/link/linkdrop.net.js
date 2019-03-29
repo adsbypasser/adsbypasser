cfc82aa8f22304517ee7599b421f838ed8a7f3b2
Version Kinda <version365@users.noreply.github.com>
2019-03-29 21:34:48 +0530

    khraba.com & adlpu.com

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -29,7 +29,7 @@
         /^(earn-url|bit-url|cut-win|link-zero|cut-earn|oturl|glory-link|coolmitten)\.com$/,
         /^(empireshort|empearn|tarnwh2i|tabeikh|yourw-ay|reb7konline|factural)\.com$/,
         /^(shrinkbuck|clkpay|linksht|haxrs|click2-url|cooltxtgen|arba7co|shortlinko)\.com$/,
-        /^(partqb2i)\.com$/,
+        /^(partqb2i|khraba|adlpu)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(www\.)?clkpays\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links|adskipme|skipurls|ely-om7)\.com$/,
