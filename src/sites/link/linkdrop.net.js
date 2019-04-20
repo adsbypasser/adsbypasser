fbcf9209173f45eb4127ed121370a88e876223d2
Version Kinda <version365@users.noreply.github.com>
2019-04-21 01:05:45 +0530

    autocarsmagz.xyz & getpocket.xyz

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -58,6 +58,7 @@
         /^(3bst|coinlink|itiurl|coshink|link5s|curs|makeurl|mooddisorder)\.co$/,
         /^(shink|shrten|gg-l|vnurl|bloggingdekh|ln11|sh11|tradeguru|newskart|kidsors)\.xyz$/,
         /^(techinhub|viralnow|shophipro|technocanvas|getfreshcloud|profitstudy)\.xyz$/,
+        /^(autocarsmagz|getpocket)\.xyz$/,
         /^(mlink|cl250|xpickle)\.club$/,
         /^(igram|gram|pingit)\.im$/,
         /^(clk|cll)\.(press|ink|sh|icu)$/,
