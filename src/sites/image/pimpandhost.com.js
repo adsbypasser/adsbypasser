fa44ef9097ee8292548e1651492ca23fae3e5c27
Version Kinda <version365@users.noreply.github.com>
2019-04-26 08:20:07 +0530

    fix for pimpandhost.com (2nd attempt) (#2996)
    * Revert "fix for pimpandhost.com (#2984)"
    
    This reverts commit 94f2b2ff0eb059cf16e985321053550092e384b7.
    
    * Update pimpandhost.com.js
    
    * Update pimpandhost.com.js
    
    fixing build failure:
    - error  Expected indentation
    - error  Mixed spaces and tabs
    
    * Update pimpandhost.com.js
    
    fixing indentations 2nd time.
    
    * Update pimpandhost.com.js
    
    close #2949

--- src/sites/image/pimpandhost.com.js
+++ src/sites/image/pimpandhost.com.js
@@ -1,10 +1,21 @@
 _.register({
   rule: {
     host: /^(www\.)?pimpandhost\.com$/,
-    path: /^\/image\//,
+    path: /^\/image\/\d+/,
+    query: /^\?size=original/,
   },
   async ready () {
     const img = $('#overflow-wrapper img.original');
     await $.openImage(img.src);
+  },
+});
+
+_.register({
+  rule: {
+    host: /^(www\.)?pimpandhost\.com$/,
+    path: /^\/image\/\d+/,
+  },
+  async start (m) {
+    await $.openLink(m.path + '?size=original');
   },
 });
