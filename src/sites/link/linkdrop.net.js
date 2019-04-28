69a2b72bc0114a7f3f994515cacf49ebc44a03b8
Version Kinda <version365@users.noreply.github.com>
2019-04-28 10:30:30 +0530

    advancedautorepairtips.me
    close #3021

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -55,6 +55,7 @@
         /^(petty|skips|tr|flaz)\.link$/,
         /^megaurl\.(in|link)$/,
         /^(adbilty|adpop|ujv|tpx|adsrt|2fly|lin65|short2win|suarankri|infotrendy)\.me$/,
+        /^advancedautorepairtips\.me$/,
         /^payskip\.(me|org)$/,
         /^(oke|cuon|cuio|cuee|cuus|cuto|cu2|linktor|flylink|uiz|cu3|uii)\.io$/,
         /^(3bst|coinlink|itiurl|coshink|link5s|curs|makeurl|mooddisorder|cutls)\.co$/,
