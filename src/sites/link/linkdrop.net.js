6648745e8c9530187c56efc716bc27a8c44a989d
Version Kinda <version365@users.noreply.github.com>
2019-05-06 21:51:26 +0530

    kutpay.com & shrt8.com & nikkarr.com & fc.lc

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -34,7 +34,7 @@
         /^(thegyaanipoint|downloaddoom|linkfay|5brgedid|earthpiclover|adigp|tomient)\.com$/,
         /^(techmen-world|razerflixs|gamesindians|dislooks|elkhbrel7sry|onaah)\.com$/,
         /^(wrap-w0rld|ommantrameditation|mawdok|techfinda|clixg|boardgift|beast-birds)\.com$/,
-        /^(talkittechy|newsinjules)\.com$/,
+        /^(talkittechy|newsinjules|kutpay|shrt8|nikkarr)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(www\.)?(clkpays|lnkjob|efshort)\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links|adskipme|skipurls|ely-om7)\.com$/,
@@ -112,6 +112,7 @@
         /^oko\.sh$/,
         /^splashnews\.ooo$/,
         /^ckk\.ai$/,
+        /^fc\.lc$/,
       ],
     },
     async ready () {
