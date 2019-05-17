5ecbc06d9074da7c745dd82a2f053af5789c0e47
Version Kinda <version365@users.noreply.github.com>
2019-05-17 14:52:02 +0530

    oploverzhome.com
    close #3069

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -34,7 +34,7 @@
         /^(thegyaanipoint|downloaddoom|linkfay|5brgedid|earthpiclover|adigp|tomient)\.com$/,
         /^(techmen-world|razerflixs|gamesindians|dislooks|elkhbrel7sry|onaah)\.com$/,
         /^(wrap-w0rld|ommantrameditation|mawdok|techfinda|clixg|boardgift|beast-birds)\.com$/,
-        /^(talkittechy|newsinjules|kutpay|nikkarr|veblink|al3amall|6aba2day)\.com$/,
+        /^(talkittechy|newsinjules|kutpay|nikkarr|veblink|al3amall|6aba2day|oploverzhome)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(www\.)?(clkpays|lnkjob|efshort)\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links|adskipme|skipurls|ely-om7)\.com$/,
