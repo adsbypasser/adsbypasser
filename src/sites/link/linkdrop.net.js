60bb7dfb3014c9d0b258878b1d0aa948f3fd6268
Version Kinda <version365@users.noreply.github.com>
2019-05-26 16:21:57 +0530

    shortzon.com
    close #3099

--- src/sites/link/linkdrop.net.js
+++ src/sites/link/linkdrop.net.js
@@ -36,7 +36,7 @@
         /^(wrap-w0rld|ommantrameditation|mawdok|techfinda|clixg|boardgift|beast-birds)\.com$/,
         /^(talkittechy|newsinjules|kutpay|nikkarr|veblink|al3amall|6aba2day|oploverzhome)\.com$/,
         /^(kooramubashir|healthfary|justlikeyojna|sarkarijobsresultss|zalipay|thefreech)\.com$/,
-        /^(hindibeen|pastekan|e7kelyana|ea-isly|aristoderas)\.com$/,
+        /^(hindibeen|pastekan|e7kelyana|ea-isly|aristoderas|shortzon)\.com$/,
         /^(vy\.)?adsvy\.com$/,
         /^(www\.)?(clkpays|lnkjob|efshort)\.com$/,
         /^(linkexa|admew|shrtfly|kuylink|cut4links|adskipme|skipurls|ely-om7)\.com$/,
