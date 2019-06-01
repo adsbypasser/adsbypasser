bf0a46c1c8e0b60218001e8e36fbdde2df27334c
Version Kinda <version365@users.noreply.github.com>
2019-06-01 22:35:19 +0530

    Update 1v.to.js

--- src/sites/link/1v.to.js
+++ src/sites/link/1v.to.js
@@ -1,10 +1,10 @@
 _.register({
-    rule: {
-      host: /^1v\.to$/,
-      path: /^\/t\/[a-zA-Z0-9/=]+/,
-    },
-    async start () {
-      const path = window.location.href.replace('/t/', '/saliendo/');
-      await $.openLink(path);
-    },
-  });
+  rule: {
+    host: /^1v\.to$/,
+    path: /^\/t\/[a-zA-Z0-9/=]+/,
+  },
+  async start () {
+    const path = window.location.href.replace('/t/', '/saliendo/');
+    await $.openLink(path);
+  },
+});
