1071f13e21fd8a88e0a08f48f6cb8645e727acf9
Call Me Nemo <callmenemo491@users.noreply.github.com>
2019-06-03 20:20:17 +0900

    Update indishare.org.js
    Trying to address the build errors:
    * 6:9   error  'btn' is never reassigned. Use 'const' instead  prefer-const
    * 8:13  error  'clkbtn' is assigned a value but never used     no-unused-vars
    * 8:42  error  Strings must use singlequote                    quotes

--- src/sites/file/indishare.org.js
+++ src/sites/file/indishare.org.js
@@ -3,9 +3,9 @@ _.register({
     host: /^(www\.)?indishare\.(org|me)$/,
   },
   async ready () {
-    let btn = $('button#downloadbtn.downloadbtn');
+    const btn = $('button#downloadbtn.downloadbtn');
     if (btn) {
-      const clkbtn = btn.removeAttribute("disabled");
+      btn.removeAttribute('disabled');
       btn.click();
     } else {
       const link = $('#content span#direct_link a');
