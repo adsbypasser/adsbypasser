a45182c70c37a49cb0ddb68b7d6e8a58647a2753
Version Kinda <version365@users.noreply.github.com>
2019-06-04 04:36:01 +0530

    bdupload.info
    close #2591

--- src/sites/file/indishare.org.js
+++ src/sites/file/indishare.org.js
@@ -1,15 +1,13 @@
 _.register({
   rule: {
-    host: /^(www\.)?indishare\.(org|me)$/,
+    host: [
+      /^(www\.)?indishare\.(org|me)$/,
+      /^bdupload\.(info|asia)$/,
+    ],
   },
   async ready () {
     const btn = $('button#downloadbtn.downloadbtn');
-    if (btn) {
-      btn.removeAttribute('disabled');
-      btn.click();
-    } else {
-      const link = $('#content span#direct_link a');
-      await $.openLink(link.href);
-    }
+    btn.removeAttribute('disabled');
+    btn.click();
   },
 });
