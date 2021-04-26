0fa82a4adb322c26a019ee9587f3478a522a10b4
Marvin <GiantTreeLP@users.noreply.github.com>
2021-04-26 13:44:04 +0200

    Add support for imgweng.xyz (#3637)
    add support for both: imgxen.xyz   imgweng.xyz

--- src/sites/image/imgvip.net.js
+++ src/sites/image/imgvip.net.js
@@ -18,6 +18,7 @@ _.register({
       host: [
         /^www\.(imgsky|imgfile|imgsee)\.net$/,
         /^www\.imagespicy\.site$/,
+        /^(imgxen|imgweng)\.xyz$/,
       ],
       path: /^\/[a-z|0-9]{4,10}$/,
     },
