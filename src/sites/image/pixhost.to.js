4e4f9b637b1805beaaac5b27dab42fb483564855
Nemo <callmenemo491@users.noreply.github.com>
2021-07-07 19:07:54 +0900

    3xplanet.com

--- src/sites/image/pixhost.to.js
+++ src/sites/image/pixhost.to.js
@@ -1,8 +1,14 @@
 _.register({
-  rule: {
-    host: /^(www\.)?pixhost\.to$/,
-    path: /^\/show\//,
-  },
+  rule: [
+      {
+        host: /^(www\.)?pixhost\.to$/,
+        path: /^\/show\//,
+      },
+      {
+        host: /^3xplanet\.com$/,
+        path: /^\/viewimage\//,
+      },
+  ],
   async ready () {
     $.remove('iframe, #ad');
 
