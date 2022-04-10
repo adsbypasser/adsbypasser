b2b3960101839da8f739295f86cefa04f0601d38
Trung Le <jackblk4@gmail.com>
2022-04-10 23:25:39 +0700

    fix: tinyurl.is

--- src/sites/link/tinyurl.is.js
+++ src/sites/link/tinyurl.is.js
@@ -2,12 +2,12 @@ _.register({
   rule: {
     host: /^tinyurl\.is$/,
   },
-  async ready () {
+  async ready() {
     // force countdown to 0 from site's eval script
-    $.window.count=0;
+    $.window.count = 0;
 
     const link = await _.tryEvery(200, () => {
-      const link = $('a[id^=skip-btn]').href;
+      const link = $('a[id^=newskip-btn]').href;
       if (link.includes('tinyurl.is')) {
         return _.none;
       } else {
