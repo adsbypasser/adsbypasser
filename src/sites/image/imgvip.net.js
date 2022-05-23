fdc5ec9624fbfcdfbf0eb1481fefd0fb254d072a
Nemo <callmenemo491@users.noreply.github.com>
2022-05-23 10:43:42 +0900

    Update imgvip.net.js

--- src/sites/image/imgvip.net.js
+++ src/sites/image/imgvip.net.js
@@ -43,7 +43,7 @@ _.register({
     {
       host: [
         /^imgkuiw\.xyz$/,
-        /^(imgwex|imgwang|imgmeno|imgwav|imgxvd|imgbbd|imgqte|imgkoc|imgkorle|imgwia)\.buzz$/,
+        /^(imgwex|imgwang|imgmeno|imgwav|imgxvd|imgbbd|imgqte|imgkoc|imgkorle|imgwia|imgizx)\.buzz$/,
       ],
       path: /^\/[a-z|0-9]{4,10}$/,
     },
