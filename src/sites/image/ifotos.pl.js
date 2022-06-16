b5465e7887d3fa2b23a797099669d3ce1e664594
Nemo <callmenemo491@users.noreply.github.com>
2022-06-16 19:56:29 +0900

    img.javstore.net

--- src/sites/image/ifotos.pl.js
+++ src/sites/image/ifotos.pl.js
@@ -3,6 +3,7 @@ _.register({
     'http://ifotos.pl/zobacz/*',
     'https://postimg.cc/*',
     'https://pixxxels.cc/*',
+    'https://img.javstore.net/image/*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
