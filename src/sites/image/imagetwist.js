3d7d7c133b19b082f3970ab7e0016c4c90a66ac3
Nemo <callmenemo491@users.noreply.github.com>
2023-07-08 13:30:00 +0900

    Update imagetwist.js
    undo change

--- src/sites/image/imagetwist.js
+++ src/sites/image/imagetwist.js
@@ -1,15 +1,4 @@
 (function () {
-
-  _.register({
-    rule: {
-      host: /^imagetwist\.netlify\.app$/,
-    },
-    async ready () {
-      const a = $('form > center > h2 > p > a');
-      await $.openLink(d.href);
-    },
-  });
-
   _.register({
     rule: [
       {
