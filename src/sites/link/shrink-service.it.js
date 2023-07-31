dd169879eaeb9c861a816cb4c891916d103d8509
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2023-07-31 09:15:31 -0300

    Improve support for shrink-service.it bypass (#3804)
    * Improve support for shrink-service.it bypass
    Based on JDownloader engine and binbox.io bypass.

--- src/sites/link/shrink-service.it.js
+++ src/sites/link/shrink-service.it.js
@@ -11,6 +11,37 @@ _.register({
 _.register({
   rule: {
     host: /^www\.shrink-service\.it$/,
+    path: /^\/btn\/(.+)/,
+  },
+  async ready (m) {
+
+    const path_id = m.path[1];
+
+    const API_URL = '//www.shrink-service.it/v3/api/prototype/init?req=init&uri=https://adshnk.com/'+path_id;
+
+    let linkInfo = await $.get(API_URL, false, {
+      Origin: _.none,
+      Referer: _.none,
+      // referrer must be here
+      Cookie: 'referrer=1',
+      'X-Requested-With': _.none,
+    });
+    linkInfo = JSON.parse(linkInfo);
+    if (!linkInfo.success) {
+      throw new $.AdsBypasserError('error when getting api information');
+    }
+
+    const urlInfo = JSON.parse(linkInfo['0'].metadata);
+    if (urlInfo.url) {
+      await $.openLink(urlInfo.url);
+      return;
+    }
+  },
+});
+
+_.register({
+  rule: {
+    host: /^www\.shrink-service\.it$/,
     path: /^\/shrinked\//,
   },
   async ready () {
