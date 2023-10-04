1afbd661a22883c2d8d5ceabec70cf76888d59f7
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-04 16:33:44 -0400

    Update imgvip.net.js

--- src/sites/image/imgvip.net.js
+++ src/sites/image/imgvip.net.js
@@ -1,47 +1,9 @@
-_.register({
-  rule: {
-    host: [
-      /^www\.(imgsky|imgfile|imgsee)\.net$/,
-      /^www\.imagespicy\.site$/,
-    ],
-    path: /^\/site\/v\/\d+$/,
-  },
-  async ready () {
-    const a = $('#myUniqueImg').parentNode;
-    await $.openLink(a.href);
-  },
-});
 
 _.register({
   rule: [
-    {
-      host: [
-        /^www\.(imgsky|imgfile|imgsee)\.net$/,
-        /^www\.imagespicy\.site$/,
-        /^(imgxen|imgweng|imgtiger|imgvivo|imgwewo|imgflyer)\.xyz$/,
-      ],
-      path: /^\/[a-z|0-9]{4,10}$/,
-    },
     {
       host: [
         /^(www\.)?imgair\.net$/,
-        /^(www\.)?imageking\.xyz$/,
-        /^www\.cloudgallery\.net$/,
-        /^kekolangti\.com$/,
-        /^imgkia\.buzz$/,
-      ],
-    },
-  ],
-  async ready () {
-    const matches = $.searchFromScripts(/document\.getElementById\("soDaBug"\)\.src = "([^"]+)";/);
-    await $.openImage(matches[1]);
-  },
-});
-
-_.register({
-  rule: [
-    {
-      host: [
         /^img[a-z]{2,6}\.(buzz|site|store|online|website|xyz|cfd|sbs)$/,
         /^(hfneiott|lgjreelqq|pyotinle|pixmtke|optiye)\.buzz$/,
         /^picuekr\.site/,
