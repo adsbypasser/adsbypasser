cb4b6c3194eeb32a1fdd17d08686c75710e4ff1a
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-04 15:54:23 -0400

    Delete keptarolo.hu.js

--- src/sites/image/keptarolo.hu.js
+++
@@ -1,9 +0,0 @@
-_.register({
-  rule: {
-    host: /^keptarolo\.hu$/,
-    path: /^(\/[^/]+\/[^/]+\.jpg)$/,
-  },
-  async start (m) {
-    await $.openImage('http://www.keptarolo.hu/kep' + m.path[1]);
-  },
-});
