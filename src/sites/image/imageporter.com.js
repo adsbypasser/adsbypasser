0fe858dfbeed8334bb31167548e61855b7572512
Nemo <callmenemo491@users.noreply.github.com>
2023-10-19 22:40:15 +0900

    Update imageporter.com.js

--- src/sites/image/imageporter.com.js
+++ src/sites/image/imageporter.com.js
@@ -2,19 +2,8 @@
 
   _.register({
     rule: {
-      host: /^(www\.)?imageporter\.com$/,
-      path: /^\/\w{12}\/.*\.html$/,
-    },
-    ready: run,
-  });
-
-  _.register({
-    rule: {
       host: [
-        /^(www\.)?(imagecarry|imagedunk|imageporter|imageswitch)\.com$/,
-        /^(www\.)?(picleet|picturedip|pictureturn)\.com$/,
         /^(www\.)?imgspice\.com$/,
-        /^(www\.)?(piclambo|yankoimages)\.net$/,
       ],
     },
     ready: run,
