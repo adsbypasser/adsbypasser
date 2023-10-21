3dc5bbe45efc5d288dd1d32d91fe8fb0c5d776bd
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-21 11:52:22 -0400

    Revert "Delete imagesnake.com.js"
    This reverts commit fa5564b54e92e64cc129d058c1483c52a18482a5.

---
+++ src/sites/image/imagesnake.com.js
@@ -0,0 +1,56 @@
+(function () {
+
+  _.register({
+    rule: [
+      {
+        host: [
+          /^www\.(freebunker|imgcarry|imgshots)\.com$/,
+          /^www\.imagesnake\.(com|org)$/,
+        ],
+        path: /^\/show\.php$/,
+        query: /^\?/,
+      },
+      {
+        host: /^www\.(freebunker|imgshots)\.com$/,
+        path: /^\/show\//,
+      },
+      {
+        host: /^imageban\.(ru|net)$/,
+        path: /^\/show\/\d{4}\/\d{2}\/\d{2}\/.+/,
+      },
+      'http://fotoo.pl/show.php?img=*.html',
+      {
+        host: /^www\.imagestime\.com$/,
+        path: /^\/show\.php\/.*\.html$/,
+      },
+    ],
+    ready: run,
+  });
+
+  _.register({
+    rule: {
+      host: [
+        /^www\.(imgcarry|imagefruit)\.com$/,
+        /^www\.pornbus\.org$/,
+        /^www\.imagesnake\.(com|org)$/,
+      ],
+      path: /^\/(img|show)\//,
+    },
+    ready: run2,
+  });
+
+  async function run () {
+    const i = $('#img_obj');
+    await $.openImage(i.src, {
+      referer: true,
+    });
+  }
+
+  async function run2 () {
+    const i = $('#img_obj');
+    await $.openImage(i.src, {
+      replace: true,
+    });
+  }
+
+})();
