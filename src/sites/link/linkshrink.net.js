8db90df72f359ba0c61bf6b53a0359059050bab4
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-24 16:29:25 -0400

    Update linkshrink.net.js
    remove inactive sites, code cleanup

--- src/sites/link/linkshrink.net.js
+++ src/sites/link/linkshrink.net.js
@@ -1,6 +1,6 @@
 _.register({
   rule: {
-    host: /^(linkshrink|lnkshrnk)\.net$/,
+    host: /^linkshrink\.net$/,
     path: /^\/[a-zA-Z0-9]+$/,
   },
   async start () {
@@ -10,34 +10,5 @@ _.register({
     let l = $.searchFromScripts(/revC\("([^"]+)"\)/);
     l = atob(l[1]);
     await $.openLink('/' + l);
-  },
-});
-
-_.register({
-  rule: {
-    host: /^(linkshrink|lnkshrnk)\.net$/,
-    path: /=(.+)$/,
-  },
-  async start (m) {
-    await $.openLink(m.path[1]);
-  },
-});
-
-_.register({
-  rule: {
-    host: /^dwindly\.io$/,
-  },
-  async ready () {
-    let l = $.searchFromScripts(/encD\("([^"]+)"\)/);
-    if (l) {
-      // second stage
-      l = atob(l[1]);
-      await $.openLink('/' + l);
-      return;
-    }
-
-    // first stage
-    l = $.searchFromScripts(/document\.location\.href = "([^"]+)"/);
-    await $.openLink(l[1]);
   },
 });
