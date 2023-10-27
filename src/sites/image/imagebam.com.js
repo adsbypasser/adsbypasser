4b485c3d7792186267a32ff2626b1dfd5ca2b26c
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-27 15:00:20 -0400

    Revert "Update imagebam.com.js"
    This reverts commit 4d92c5e301fda28f630a6b23fabd4a47c46bb196.

--- src/sites/image/imagebam.com.js
+++ src/sites/image/imagebam.com.js
@@ -3,8 +3,13 @@ _.register({
     host: /^www\.imagebam\.com$/,
     path: /^\/(view|image)\/.*$/,
   },
-   async ready () {
-      const img = $('img.main-image');
-      await $.openImage(img.src);
-    },
-  });
+  async ready () {
+    let o = $.$('div.view-navigation > a:nth-child(3)');
+    if (o) {
+      await $.openImage(o.href);
+      return;
+    }
+    o = $('#app > main > div > h2 > a');
+    await $.openLink(o.href);
+  },
+});
