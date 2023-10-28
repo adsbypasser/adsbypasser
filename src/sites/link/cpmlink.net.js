c1988ede68d4247e0d215c01549d35fa4aaadfcf
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-28 08:45:54 -0400

    Update and rename fas.li.js to cpmlink.net.js

---
+++ src/sites/link/cpmlink.net.js
@@ -0,0 +1,29 @@
+
+_.register({
+  rule: [
+    {
+      host: /^cpmlink\.net$/,
+      path: /^\/go\/[\w-]+$/,
+    },
+  ],
+  async ready () {
+    let a = $('#btn-main');
+    const i = a.href.lastIndexOf('http');
+    a = a.href.substr(i);
+    await $.openLink(a);
+  },
+});
+
+_.register({
+  rule: {
+    host: [
+      /^shon\.xyz$/,
+      /^(www\.)?shink\.me$/,
+    ],
+    path: /^\/[\w-]+$/,
+  },
+  async ready () {
+    const f = $('#skip');
+    f.submit();
+  },
+});
