8b1852bd43611d4e003a169c228ff29608919034
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-10-31 12:58:46 -0400

    Update shortlinkto.site.js

--- src/sites/link/shortlinkto.site.js
+++ src/sites/link/shortlinkto.site.js
@@ -1,9 +1,9 @@
 _.register({
   rule: {
-  host: /^shortlinkto\.site$/,
-},
-async ready () {
-  const b = $('.btn.btn-primary.btn-block');
-  b.click();
- },
+    host: /^shortlinkto\.site$/,
+  },
+  async ready () {
+    const b = $('.btn.btn-primary.btn-block');
+    b.click();
+  },
 });
