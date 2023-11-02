a6df4c39f05536355e034e12d6f36e788171b957
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-02 10:02:10 -0400

    Create apunkasoftware.net.js (#3889)

---
+++ src/sites/file/apunkasoftware.net.js
@@ -0,0 +1,19 @@
+_.register({
+  rule: {
+    host: /^(www\.)?apunkasoftware\.net$/,
+  },
+  async ready () {
+    const a = $('div#proceed-now > a#dlink');
+    await $.openLink(a.href);
+  },
+});
+
+_.register({
+  rule: {
+    host: /^thefileslocker\.net$/,
+  },
+  async ready () {
+    const button = $('#downloadbtn');
+    button.click();
+  },
+});
