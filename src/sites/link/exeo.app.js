cacd73bca9ce80b6b18ffcf970fb51799477e34f
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-04 23:47:11 -0400

    Create exeo.app.js (#3894)

---
+++ src/sites/link/exeo.app.js
@@ -0,0 +1,15 @@
+_.register({
+  rule: {
+    host: /^exeo\.app$/,
+  },
+  async ready () {
+    const a = $('.link-button.button');
+    a.click();
+    await _.wait(2000);
+    const b = $('.link-button');
+    b.click();
+    await _.wait(6000);
+    const c = $('.link-button.get-link');
+    c.click();
+  },
+});
