84ed3ad7b40790d1203133a99cdf849f3caa1650
Nemo <callmenemo491@users.noreply.github.com>
2023-11-05 12:53:52 +0900

    Create imgbase.ru.js

---
+++ src/sites/image/imgbase.ru.js
@@ -0,0 +1,15 @@
+_.register({
+  rule: {
+    host: [
+      /^imgbase\.ru$/,
+    ],
+    path: /^\/\d+\/\d+\/$/,
+  },
+  async ready () {
+    let i = $('#pay_thumb_img img');
+    i = i.getAttribute('onclick');
+    i = i.match(/mshow\('(.+)'\)/);
+    i = i[1];
+    await $.openImage(i);
+  },
+});
