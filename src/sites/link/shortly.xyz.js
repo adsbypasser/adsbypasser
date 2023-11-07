a1c3daaddac351e865013ab6bc727bf81bf47e2a
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-07 17:56:24 -0500

    Create shortly.xyz.js

---
+++ src/sites/link/shortly.xyz.js
@@ -0,0 +1,10 @@
+_.register({
+  rule: {
+    host: /^(www\.)?shortly\.xyz$/,
+  },
+  async ready () {
+    await _.wait(8000);
+    const a = $('.btn-success.btn-lg.get-link');
+    a.click();
+  },
+});
