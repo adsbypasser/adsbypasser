3984b7f9fcde15886a1d6e3890366760cf569565
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-10 08:02:13 -0500

    Create ak.sv.js (#3908)

---
+++ src/sites/file/ak.sv.js
@@ -0,0 +1,13 @@
+_.register({
+  rule: {
+    host: /^ak\.sv$/,
+  },
+  async ready () {
+    await _.wait(1000);
+    const any = $('html');
+    any.click();
+    await _.wait(6000);
+    const a = $('a[class="download_button"]');
+    await $.openLink(a);
+  },
+});
