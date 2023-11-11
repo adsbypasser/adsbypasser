06e87bdc9b9fc91faa34cdde9aa2c6de468d5026
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-11 14:46:07 -0500

    Create techgeek.digital.js

---
+++ src/sites/link/techgeek.digital.js
@@ -0,0 +1,15 @@
+_.register({
+  rule: {
+    host: /^techgeek\.digital$/,
+  },
+  async ready () {
+    const b = $('[class="btn-main get-link"]');
+    if (b) {
+      b.click();
+    }
+    const a = $('a[class="btn-main get-link"]');
+    if (a) {
+      await $.openLink(a.href);
+    }
+  },
+});
