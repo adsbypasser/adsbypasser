9064eb97ed6b774923204ae85d60b69f73569eff
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-11 14:21:58 -0500

    Create tnshort.net.js

---
+++ src/sites/link/tnshort.net.js
@@ -0,0 +1,10 @@
+_.register({
+  rule: {
+    host: /^go\.tnshort\.net$/,
+  },
+  async ready () {
+    await _.wait(3000);
+    const a = $('a[class="btn btn-success btn-lg get-link"]');
+    await $.openLink(a.href);
+  },
+});
