de836cc984390dc5464c6f1ffc00bf61916eb1e9
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-11-20 15:58:51 -0500

    Create tutwuri.id.js

---
+++ src/sites/link/tutwuri.id.js
@@ -0,0 +1,15 @@
+_.register({
+  rule: {
+    host: /^tutwuri\.id$/,
+  },
+  async ready () {
+    await _.wait(1000);
+    const a = $('#btn-1');
+    a.click();
+    await _.wait(12000);
+    const b = $('#btn-2');
+    b.click();
+    const c = $('#btn-3');
+    c.click();
+  },
+});
