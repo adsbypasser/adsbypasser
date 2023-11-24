050b9747f4add88022d3bb5519cfb5c329f0a682
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-11-24 16:03:32 +0900

    feat: ai18.pics

--- src/sites/image/ifotos.pl.js
+++ src/sites/image/ifotos.pl.js
@@ -2,8 +2,13 @@ _.register({
   rule: [
     'https://img.javstore.net/image/*',
     'https://picnew.space/image/*',
-    'https://pig69.com/*',
-    'https://javball.com/*'
+    'https://pig69.com/upload/en/*',
+    'https://javball.com/upload/en/*',
+    'https://ai18.pics/upload/en/*',
+    'https://cnxx.me/upload/en/*',
+    'https://cnpics.org/upload/en/*',
+    'https://cosplay18.pics/upload/en/*',
+    'https://porn4f.com/upload/en/*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
