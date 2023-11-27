951a8996ac07d16b9acb7967221f23494468a57d
Nemo <callmenemo491@users.noreply.github.com>
2023-11-27 20:49:06 +0900

    Update javstore.net.js
    minor alphabetical reorder

--- src/sites/image/javstore.net.js
+++ src/sites/image/javstore.net.js
@@ -1,16 +1,16 @@
 _.register({
   rule: [
+    'https://ai18.pics/upload/en/*',
+    'https://cnpics.org/upload/en/*',
+    'https://cnxx.me/upload/en/*',
+    'https://cosplay18.pics/upload/en/*',
+    'https://idol69.net/upload/en/*',
     'https://img.javstore.net/image/*',
+    'https://javball.com/upload/en/*',
+    'https://ovabee.com/upload/en/*',
     'https://picnew.space/image/*',
     'https://pig69.com/upload/en/*',
-    'https://javball.com/upload/en/*',
-    'https://ai18.pics/upload/en/*',
-    'https://cnxx.me/upload/en/*',
-    'https://cnpics.org/upload/en/*',
-    'https://cosplay18.pics/upload/en/*',
     'https://porn4f.com/upload/en/*',
-    'https://ovabee.com/upload/en/*',
-    'https://idol69.net/upload/en/*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
