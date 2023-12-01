15b967f9fbee927e60bca09c314a0b560f52378d
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-01 17:32:35 -0500

    Create picturedent.org.js

---
+++ src/sites/image/picturedent.org.js
@@ -0,0 +1,14 @@
+_.register({
+  rule: {
+    host: [
+      /^picturedent\.org$/,
+      /^everest\.picturedent\.org$/,
+      /^pacific\.picturedent\.org$/,
+    ],
+    path: /^\/image\//,
+  },
+  async ready () {
+    const i = $('#full_img');
+    await $.openImage(i.src);
+  },
+});
