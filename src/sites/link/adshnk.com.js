9b8d6b17485f9fe9d0a3d8740a4f69b246a6f1f1
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-02 22:39:48 -0500

    Create adshnk.com.js (#3963)

---
+++ src/sites/link/adshnk.com.js
@@ -0,0 +1,14 @@
+_.register({
+  rule: {
+    host: /^adshnk\.com$/,
+  },
+  async ready () {
+    await _.wait(16000);
+    const b = $('button[class="ui right labeled icon button primary huge fluid"]');
+    b.click();
+
+    await _.wait(18000);
+    const a = $('a[id="final_redirect"]');
+    await $.openLink(a.href);
+  },
+});
