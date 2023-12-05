6a2443fb2c389c69fdf9be6a0a7a69ce34cb6a36
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-05 17:16:27 -0500

    Update adfoc.us.js

--- src/sites/link/adfoc.us.js
+++ src/sites/link/adfoc.us.js
@@ -1,22 +1,9 @@
 _.register({
-  rule: 'http://adfoc.us/*',
+  rule: {
+    host: /^adfoc\.us$/,
+  },
   async ready () {
-    const promise = new Promise((resolve) => {
-      const root = document.body;
-      const observer = new MutationObserver(() => {
-        let o = $.$('#showSkip');
-        if (o) {
-          observer.disconnect();
-          o = o.querySelector('a');
-          resolve(o.href);
-        }
-      });
-      observer.observe(root, {
-        childList: true,
-        subtree: true,
-      });
-    });
-    const url = await promise;
-    await $.openLink(url);
+    const a = $('.skip');
+    await $.openLink(a.href);
   },
 });
