f0a4816cdd8eb37e85c139e4f69fcd3127dbc82b
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-09 08:28:40 -0500

    Create fc.lc.js

---
+++ src/sites/link/fc.lc.js
@@ -0,0 +1,28 @@
+_.register({
+  rule: {
+    host: /^fc-lc\.(com|xyz)$/,
+  },
+  async ready () {
+    await _.wait(2000);
+    const b = $('.btn-primary.btn-captcha.mb-4');
+    b.click();
+  },
+});
+
+_.register({
+  rule: {
+    host: /^loaninsurehub\.com$/,
+  },
+  async ready () {
+    const b = $('#glink');
+    if (b) {
+      b.click();
+    }
+    await _.wait(12000);
+    $.remove('#overlay');
+    const a = $('#surl');
+    if (a) {
+      a.click();
+    }
+  },
+});
