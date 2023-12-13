529f329f06819dffded45c8b29729fa089ebe0bd
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-13 10:46:11 -0500

    Create imgdrive.net.js

---
+++ src/sites/image/imgdrive.net.js
@@ -0,0 +1,13 @@
+_.register({
+  rule: {
+    host: [
+      /^(www\.)?imgdrive\.net$/,
+      /^(www\.)?(imgtaxi|imgwallet|imgadult)\.com$/,
+    ],
+  },
+  async ready () {
+    let m = $('meta[property="og:image"]');
+    m = m.content.replace('small', 'big');
+    await $.openImage(m);
+  },
+});
