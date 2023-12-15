4c43f182471df459fc06da8b560d90b9b5cb84e8
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-15 17:56:23 -0500

    Update imageban.ru.js

--- src/sites/image/imageban.ru.js
+++ src/sites/image/imageban.ru.js
@@ -1,19 +1,11 @@
-(function () {
-
-  _.register({
-    rule: 
+_.register({
+  rule: 
       {
         host: /^imageban\.ru$/,
-        path: /^\/show\/\d{4}\/\d{2}\/\d{2}\/.+/,
       },
-    ready: run,
-  });
 
-  async function run () {
-    const i = $('#img_obj');
-    await $.openImage(i.src, {
-      referer: true,
-    });
+  async ready () {
+    const i = $('#img_main');
+    await $.openImage(i.src);
   }
-
-})();
+});
