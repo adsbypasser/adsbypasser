c1986aa28f199d907dc3465a0fcdafa2949157f1
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-16 16:26:41 -0500

    Update imgbox.com.js

--- src/sites/image/imgbox.com.js
+++ src/sites/image/imgbox.com.js
@@ -1,10 +1,8 @@
 _.register({
   rule: {
     host: /^imgbox\.com$/,
-    path: /^\/[\d\w]+$/,
   },
   async ready () {
-    $.remove('iframe');
     const i = $('#img');
     await $.openImage(i.src);
   },
