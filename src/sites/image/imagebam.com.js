9b90ae70add32647632d3fc9a7b79da6d67f74e6
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-16 15:46:36 -0500

    Update imagebam.com.js

--- src/sites/image/imagebam.com.js
+++ src/sites/image/imagebam.com.js
@@ -1,14 +1,9 @@
 _.register({
   rule: {
     host: /^www\.imagebam\.com$/,
-    path: /^\/(view|image)\/.*$/,
   },
   async ready () {
-    let a = $('#continue > a');
-    if (a) {
-      a.click();
-    }
-    a = $('img.main-image');
-    await $.openImage(a.src);
+    const img = $('img.main-image');
+    await $.openImage(img.src);
   },
 });
