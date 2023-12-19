f7114ea67439e147318f3c05991e8e05e724bffa
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-19 13:40:22 -0500

    Update 1ink.cc.js

--- src/sites/link/1ink.cc.js
+++ src/sites/link/1ink.cc.js
@@ -1,14 +1,9 @@
 _.register({
   rule: {
     host: /^1ink\.cc$/,
-    path: /^\/\w+$/,
   },
   async ready () {
-    let url = document.head.querySelector('[name=keywords]').content;
-    const urlCheck = url.match(/^https?:\/\//);
-    if (!urlCheck) {
-      url = 'http://' + url;
-    }
-    await $.openLink(url);
+    const a = $('#countingbtn');
+    await $.openLink(a.href);
   },
 });
