a2c358a5671857d6dd0d3f8a39e4cc66a952f3a2
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2023-12-31 16:32:10 -0500

    Update 22pixx.xyz.js

--- src/sites/image/22pixx.xyz.js
+++ src/sites/image/22pixx.xyz.js
@@ -12,6 +12,17 @@ _.register({
 _.register({
   rule: {
     host: /^22pixx\.xyz$/,
+    path: /^\/ib-[aior]\/(.+)\.jpeg\.html/,
+  },
+  async start () {
+    const path = window.location.href.replace('/ib-', '/').replace('.html', '');
+    await $.openLink(path);
+  },
+});
+
+_.register({
+  rule: {
+    host: /^22pixx\.xyz$/,
     path: /^\/i-[1a]\/(.+)\.jpeg\.html/,
   },
   async start () {
@@ -34,7 +45,7 @@ _.register({
 _.register({
   rule: {
     host: /^22pixx\.xyz$/,
-    path: /^\/y-[aio1]\/(.+)\.jpeg\.html/,
+    path: /^\/y-[1aio]\/(.+)\.jpeg\.html/,
   },
   async start () {
     const path = window.location.href.replace('/y-', '/').replace('.html', '');
