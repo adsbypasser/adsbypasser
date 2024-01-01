da678942a074b36e241aaf3ecd0b353dd8667813
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2024-01-01 16:31:09 -0500

    Update javstore.net.js

--- src/sites/image/javball.com.js
+++ src/sites/image/javball.com.js
@@ -5,13 +5,10 @@ _.register({
     'https://cnxx.me/upload/en/*',
     'https://cosplay18.pics/upload/en/*',
     'https://idol69.net/upload/en/*',
-    'https://img.javstore.net/image/*',
     'https://javball.com/upload/en/*',
     'https://ovabee.com/upload/en/*',
-    'https://picnew.space/image/*',
     'https://pig69.com/upload/en/*',
     'https://porn4f.com/upload/en/*',
-    'https://xxxaddicted.top/image/*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
