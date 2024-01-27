f74996e709f92f33b5f159df2dd03a81bf57b01f
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-01-27 17:51:44 -0300

    Add otomi-games.com support

---
+++ src/sites/link/otomi-games.com.js
@@ -0,0 +1,10 @@
+_.register({
+  rule: {
+    host: /^otomi-games\.com$/,
+    path: /^\/go\//
+  },
+  async ready () {
+    const ma = $('#wpsafe-link a');
+    await $.openLink(ma.href);
+  },
+});
