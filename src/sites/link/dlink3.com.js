c42bf67859a458d1625d3e97d53ee07ee7a98a09
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2024-01-31 14:04:14 -0500

    Create dlink3.com.js

---
+++ src/sites/link/dlink3.com.js
@@ -0,0 +1,10 @@
+_.register({
+  rule: {
+    host: /^www\.dlink3\.com$/,
+  },
+  async ready () {
+    await _.wait(12000);
+    const a = $('[class="myButton"]');
+    await $.openLink(a.href);
+  },
+});
