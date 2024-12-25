4e99a40b6d7d647b2f896b48fc501cbb41caf305
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2024-12-25 17:52:10 -0300

    Add techstudify.com bypass

---
+++ src/sites/link/techstudify.com.js
@@ -0,0 +1,11 @@
+_.register({
+  rule:
+    {
+      host: /^techstudify\.com$/,
+      path: /^\/elon.php/,
+      query: /link=([^&]+)/,
+    },
+  async start (m) {
+    await $.openLink('https://rplinks.in/' + m.query[1]);
+  },
+});
