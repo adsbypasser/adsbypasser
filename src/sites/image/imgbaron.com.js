0db0d80508c598bf208b3343a17719f877f46ff9
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-02-25 08:14:35 -0500

    Update fotokiz.com.js (#4079)

--- src/sites/image/imgbaron.com.js
+++ src/sites/image/imgbaron.com.js
@@ -1,27 +1,15 @@
 _.register({
   rule: {
     host: [
-      /^crownimg\.com$/,
-      /^fotokiz\.com$/,
       /^imgbaron\.com$/,
-      /^imgsen\.com$/,
       /^imgsto\.com$/,
-      /^kropic\.com$/,
-      /^kvador\.com$/,
-      /^picbaron\.com$/,
-      /^picdollar\.com$/,
-      /^pics4upload\.com$/,
       /^silverpic\.com$/,
-      /^barbit\.net$/,
-      /^pics4you\.net$/,
-      /^imgstar\.eu$/,
       /^www\.fappic\.com$/,
     ],
   },
   async ready () {
     const i = $.$('img.pic');
     if (i) {
-      // second stage
       await $.openImage(i.src);
       return;
     }
