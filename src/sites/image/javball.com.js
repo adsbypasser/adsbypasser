1c4a9a63153e013044bd0eae68e49b056782dcc7
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-03-20 15:34:00 +0900

    feat: 3minx.com
    * 3minx.com
    * fikfok.net
    * hentaicovid.com
    * hentaipig.com
    * porn4f.org
    * sweetie-fox.com

--- src/sites/image/javball.com.js
+++ src/sites/image/javball.com.js
@@ -1,11 +1,15 @@
 _.register({
   rule: [
+    'https://3minx.com/upload/en/*',
     'https://4fuk.me/upload/en/*',
     'https://555fap.com/upload/en/*',
     'https://ai18.pics/upload/en/*',
     'https://cnpics.org/upload/en/*',
     'https://cnxx.me/upload/en/*',
     'https://cosplay18.pics/upload/en/*',
+    'https://fikfok.net/upload/en/*',
+    'https://hentaicovid.com/uploads/en/*',
+    'https://hentaipig.com/upload/en/*',
     'https://idol69.net/upload/en/*',
     'https://javball.com/upload/en/*',
     'https://javsunday.com/upload/en/*',
@@ -13,6 +17,8 @@ _.register({
     'https://ovabee.com/upload/en/*',
     'https://pig69.com/upload/en/*',
     'https://porn4f.com/upload/en/*',
+    'https://porn4f.org/upload/en/*',
+    'https://sweetie-fox.com/upload/en/*',
   ],
   async ready () {
     const m = $('meta[property="og:image"]');
