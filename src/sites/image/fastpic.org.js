faf75fba28715af283d8e6d215c2d14e2ba912af
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-07-12 11:32:45 -0400

    Update fastpic.org.js

--- src/sites/image/fastpic.org.js
+++ src/sites/image/fastpic.org.js
@@ -8,7 +8,7 @@ _.register({
       await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.openLink(a.href);
       return;
     }
-    i = (0,_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$)('.image');
-    await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.openImage(i.src);
+    a = (0,_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$)('.image');
+    await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.openImage(a.src);
   },
 });
