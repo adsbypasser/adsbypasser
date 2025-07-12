8bbb6721d13267f608ecd0b90f817ea26a4c39f2
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-07-12 11:35:52 -0400

    Update fastpic.org.js

--- src/sites/image/fastpic.org.js
+++ src/sites/image/fastpic.org.js
@@ -3,12 +3,12 @@ _.register({
     host: /^fastpic\.org$/,
   },
   async ready () {
-    let a = _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.$('#imglink');
+    let a = $.$('#imglink');
     if (a) {
-      await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.openLink(a.href);
+      await $.openLink(a.href);
       return;
     }
-    a = (0,_ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$)('.image');
-    await _ADSBYPASSER_NAMESPACE___WEBPACK_IMPORTED_MODULE_0__.$.openImage(a.src);
+    a = $('.image');
+    await $.openImage(a.src);
   },
 });
