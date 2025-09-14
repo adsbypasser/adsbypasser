ccc92a62f4d72db9f621c92c273ade431d95cfd1
Nemo <callmenemo491@users.noreply.github.com>
2025-09-14 12:33:47 +0900

    Update cookie.js

--- src/util/cookie.js
+++ src/util/cookie.js
@@ -1,42 +1,49 @@
-import { none, forEach, find } from "util/core.js";
+import { none, forEach, find } from 'util/core.js';
 
+// -----------------------------
+// Set a cookie
+// -----------------------------
 function setCookie(key, value) {
   document.cookie = `${key}=${value};path=${location.pathname};`;
 }
 
+// -----------------------------
+// Get a cookie
+// -----------------------------
 function getCookie(key) {
-  let [, c] = find(document.cookie.split(";"), (v) => {
-    const k = v.replace(/^\s*([a-zA-Z0-9-_]+)=.+$/, "$1");
-    if (k !== key) {
-      return none;
-    }
+  const [, c] = find(document.cookie.split(';'), v => {
+    const k = v.replace(/^\s*([a-zA-Z0-9-_]+)=.+$/, '$1');
+    if (k !== key) return none;
   });
-  if (c === none) {
-    return null;
-  }
-  c = c.replace(/^\s*[a-zA-Z0-9-_]+=([^;]+).?$/, "$1");
-  if (!c) {
-    return null;
-  }
-  return c;
+
+  if (c === none) return null;
+
+  const value = c.replace(/^\s*[a-zA-Z0-9-_]+=([^;]+).?$/, '$1');
+  return value || null;
 }
 
+// -----------------------------
+// Reset all cookies for domain variants
+// -----------------------------
 function resetCookies() {
-  const a = document.domain;
-  const b = document.domain.replace(/^www\./, "");
-  const c = document.domain.replace(/^(\w+\.)+?(\w+\.\w+)$/, "$2");
-  const d = new Date(1e3).toUTCString();
+  const domainFull = document.domain;
+  const domainNoWWW = document.domain.replace(/^www\./, '');
+  const domainRoot = document.domain.replace(/^(\w+\.)+?(\w+\.\w+)$/, '$2');
+  const expires = new Date(1e3).toUTCString();
 
-  forEach(document.cookie.split(";"), (v) => {
-    const k = v.replace(/^\s*(\w+)=.+$/, "$1");
+  const set = (k, d, exp) => `${k}=;path=/;domain=${d};expires=${exp};`;
 
-    document.cookie = `${k}=;expires=${d};`;
-    document.cookie = `${k}=;path=/;expires=${d};`;
-    const e = (a, b, c) => `${a}=;path=/;domain=${b};expires=${c};`;
-    document.cookie = e(k, a, d);
-    document.cookie = e(k, b, d);
-    document.cookie = e(k, c, d);
+  forEach(document.cookie.split(';'), v => {
+    const key = v.replace(/^\s*(\w+)=.+$/, '$1');
+
+    // Delete with various paths/domains
+    document.cookie = `${key}=;expires=${expires};`;
+    document.cookie = `${key}=;path=/;expires=${expires};`;
+    document.cookie = set(key, domainFull, expires);
+    document.cookie = set(key, domainNoWWW, expires);
+    document.cookie = set(key, domainRoot, expires);
   });
 }
 
+// -----------------------------
 export { setCookie, getCookie, resetCookies };
