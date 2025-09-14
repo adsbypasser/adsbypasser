24d351592b27ad90e414d93531a33ecbb5e9e18f
Nemo <callmenemo491@users.noreply.github.com>
2025-09-14 12:53:36 +0900

    Update logger.js

--- src/util/logger.js
+++ src/util/logger.js
@@ -2,28 +2,22 @@ import { isString } from 'util/core.js';
 
 const quiet = false;
 
-// -----------------------------
-// Internal logging helper
-// -----------------------------
 function log(method, args) {
   if (quiet) return;
 
-  const argsArray = Array.prototype.slice.call(args);
-  if (isString(argsArray[0])) {
-    argsArray[0] = 'AdsBypasser: ' + argsArray[0];
+  args = Array.prototype.slice.call(args);
+  if (isString(args[0])) {
+    args[0] = 'AdsBypasser: ' + args[0];
   } else {
-    argsArray.unshift('AdsBypasser:');
+    args.unshift('AdsBypasser:');
   }
 
-  const consoleMethod = console[method];
-  if (typeof consoleMethod === 'function') {
-    consoleMethod.apply(console, argsArray);
+  const fn = console[method];
+  if (typeof fn === 'function') {
+    fn.apply(console, args);
   }
 }
 
-// -----------------------------
-// Public logging functions
-// -----------------------------
 function debug() {
   log('debug', arguments);
 }
