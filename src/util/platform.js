e8b95fa4b18db6b7546f0b9f366d8888337483d4
Nemo <callmenemo491@users.noreply.github.com>
2025-09-14 13:04:16 +0900

    Update platform.js

--- src/util/platform.js
+++ src/util/platform.js
@@ -8,12 +8,12 @@ function getUnsafeWindow() {
   let w = null;
   try {
     w = unsafeWindow;
-  } catch (e) {
+  } catch {
     try {
       // eslint-disable-next-line no-eval
       w = (0, eval)('this').global;
-    } catch (e) {
-      // eslint-disable-line no-unused-vars
+    } catch {
+      // fallback failed
     }
   }
   // eslint-disable-next-line no-eval
@@ -21,50 +21,39 @@ function getUnsafeWindow() {
 }
 
 function getGreaseMonkeyAPI() {
-  if (rawUSW.global) {
-    return null;
-  }
+  if (rawUSW.global) return null;
 
   const gm = {};
 
-  if (typeof GM_openInTab === 'function') {
-    gm.openInTab = GM_openInTab;
-  } else {
-    gm.openInTab = GM.openInTab;
-  }
+  gm.openInTab =
+    typeof GM_openInTab === 'function' ? GM_openInTab : GM.openInTab;
 
-  if (typeof GM_getValue === 'function') {
-    gm.getValue = (name, default_) => Promise.resolve(GM_getValue(name, default_));
-  } else {
-    gm.getValue = GM.getValue;
-  }
+  gm.getValue =
+    typeof GM_getValue === 'function'
+      ? (name, default_) => Promise.resolve(GM_getValue(name, default_))
+      : GM.getValue;
 
-  if (typeof GM_setValue === 'function') {
-    gm.setValue = (name, value) => Promise.resolve(GM_setValue(name, value));
-  } else {
-    gm.setValue = GM.setValue;
-  }
+  gm.setValue =
+    typeof GM_setValue === 'function'
+      ? (name, value) => Promise.resolve(GM_setValue(name, value))
+      : GM.setValue;
 
-  if (typeof GM_deleteValue === 'function') {
-    gm.deleteValue = (name) => Promise.resolve(GM_deleteValue(name));
-  } else {
-    gm.deleteValue = GM.deleteValue;
-  }
+  gm.deleteValue =
+    typeof GM_deleteValue === 'function'
+      ? (name) => Promise.resolve(GM_deleteValue(name))
+      : GM.deleteValue;
 
-  if (typeof GM_xmlhttpRequest === 'function') {
-    gm.xmlHttpRequest = GM_xmlhttpRequest;
-  } else {
-    gm.xmlHttpRequest = GM.xmlHttpRequest;
-  }
+  gm.xmlHttpRequest =
+    typeof GM_xmlhttpRequest === 'function'
+      ? GM_xmlhttpRequest
+      : GM.xmlHttpRequest;
 
-  if (typeof GM_registerMenuCommand === 'function') {
-    gm.registerMenuCommand = GM_registerMenuCommand;
-  } else {
-    gm.registerMenuCommand = nop;
-  }
+  gm.registerMenuCommand =
+    typeof GM_registerMenuCommand === 'function' ? GM_registerMenuCommand : nop;
 
   if (typeof GM_getResourceURL === 'function') {
-    gm.getResourceUrl = (resourceName) => Promise.resolve(GM_getResourceURL(resourceName));
+    gm.getResourceUrl = (resourceName) =>
+      Promise.resolve(GM_getResourceURL(resourceName));
   } else if (typeof GM === 'object' && GM && GM.getResourceUrl) {
     gm.getResourceUrl = GM.getResourceUrl;
   }
@@ -73,11 +62,8 @@ function getGreaseMonkeyAPI() {
 }
 
 function getGMInfo() {
-  if (typeof GM_info === 'object' && GM_info) {
-    return GM_info;
-  } else if (typeof GM === 'object' && GM && GM.info) {
-    return GM.info;
-  }
+  if (typeof GM_info === 'object' && GM_info) return GM_info;
+  if (typeof GM === 'object' && GM && GM.info) return GM.info;
   return {};
 }
 
@@ -85,45 +71,31 @@ const MAGIC_KEY = '__adsbypasser_reverse_proxy__';
 
 function getUnsafeWindowProxy() {
   const isGreaseMonkey = getGMInfo().scriptHandler === 'Greasemonkey';
-  if (!isGreaseMonkey) {
-    return rawUSW;
-  }
+  if (!isGreaseMonkey) return rawUSW;
 
   const decorator = {
     set(target, key, value) {
-      if (key === MAGIC_KEY) {
-        return false;
-      } else {
-        target[key] = clone(value);
-      }
+      if (key === MAGIC_KEY) return false;
+      target[key] = clone(value);
       return true;
     },
     get(target, key) {
       if (key === MAGIC_KEY) return target;
-
       const value = target[key];
       const type = typeof value;
-      if (value === null || (type !== 'function' && type !== 'object')) {
-        return value;
-      }
+      if (value === null || (type !== 'function' && type !== 'object')) return value;
       return new Proxy(value, decorator);
     },
     apply(target, self, args) {
       args = Array.prototype.slice.call(args);
 
-      if (target === unsafeWindow.Object.defineProperty) {
-        args[0] = args[0][MAGIC_KEY];
-      }
+      if (target === unsafeWindow.Object.defineProperty) args[0] = args[0][MAGIC_KEY];
       if (target === unsafeWindow.Function.apply) {
         self = self[MAGIC_KEY];
         args[1] = Array.prototype.slice.call(args[1]);
       }
-      if (target === unsafeWindow.document.querySelector) {
-        self = self[MAGIC_KEY];
-      }
-      if (target === unsafeWindow.document.write) {
-        self = self[MAGIC_KEY];
-      }
+      if (target === unsafeWindow.document.querySelector) self = self[MAGIC_KEY];
+      if (target === unsafeWindow.document.write) self = self[MAGIC_KEY];
 
       const usargs = clone(args);
       return target.apply(self, usargs);
@@ -141,23 +113,14 @@ function getUnsafeWindowProxy() {
 }
 
 function clone(safe) {
-  if (safe === null || !(safe instanceof Object)) {
-    return safe;
-  }
-  if (safe === unsafeWindow) {
-    return safe;
-  }
-  if (safe instanceof String) {
-    return safe.toString();
-  }
-  if (safe instanceof Function) {
+  if (safe === null || !(safe instanceof Object)) return safe;
+  if (safe === unsafeWindow) return safe;
+  if (safe instanceof String) return safe.toString();
+  if (safe instanceof Function)
     return exportFunction(safe, unsafeWindow, { allowCrossOriginArguments: true });
-  }
   if (safe instanceof Array) {
     const unsafe = new unsafeWindow.Array();
-    for (let i = 0; i < safe.length; i++) {
-      unsafe.push(clone(safe[i]));
-    }
+    for (let i = 0; i < safe.length; i++) unsafe.push(clone(safe[i]));
     return unsafe;
   }
 
