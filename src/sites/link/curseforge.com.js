ef1a1270f42016aba41431c7e5ad1a5fd9a9c5d7
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-10-05 04:07:01 +0900

    chore: npm run format

--- src/sites/link/curseforge.com.js
+++ src/sites/link/curseforge.com.js
@@ -5,11 +5,11 @@ _.register({
   rule: {
     host: /^www\.curseforge\.com$/,
   },
-  async ready () {
+  async ready() {
     await _.wait(1000);
     const b = $(".btn-cta.download-btn");
     b.click();
-    
+
     await _.wait(1000);
     const c = $(".btn-lined.download-btn");
     c.click();
