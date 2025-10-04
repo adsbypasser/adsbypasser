15a747d2d1f1ccd06cb64b45b474bfb1db36c74b
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-10-04 17:41:09 -0300

    Add network-loop.com support

---
+++ src/sites/link/network-loop.com.js
@@ -0,0 +1,16 @@
+/**
+ * @domain network-loop.com
+ */
+_.register({
+  rule: {
+    host: /^network-loop\.com$/,
+    query: /u=([^&]+)/,
+  },
+  async start(m) {
+    await _.wait(3000);
+    const shadowHost = document.querySelector('#print_button');
+    const shadowRoot = shadowHost.shadowRoot;
+    const buttonInShadow = shadowRoot.querySelector('a#pb_2');
+    await $.openLink(buttonInShadow.href);
+  },
+});
