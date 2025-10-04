5ee3ac49aa982b3321822873ed09598fd1678284
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-10-04 13:29:33 -0400

    feat: curseforge.com
    Closes #4069

---
+++ src/sites/link/curseforge.com.js
@@ -0,0 +1,17 @@
+/**
+ * @domain curseforge.com
+ */
+_.register({
+  rule: {
+    host: /^www\.curseforge\.com$/,
+  },
+  async ready () {
+    await _.wait(1000);
+    const b = $(".btn-cta.download-btn");
+    b.click();
+    
+    await _.wait(1000);
+    const c = $(".btn-lined.download-btn");
+    c.click();
+  },
+});
