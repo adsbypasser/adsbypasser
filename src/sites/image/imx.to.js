aec8d5fbf71e820c4ef2dafc4e8e0cc931711be5
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-10-07 13:48:07 -0400

    retire: acidimg.cc

--- src/sites/image/imx.to.js
+++ src/sites/image/imx.to.js
@@ -1,20 +1,14 @@
 /**
- * @domain acidimg.cc
  * @domain imx.to
  */
 _.register({
   rule: {
-    host: [/^acidimg\.cc$/, /^imx\.to$/],
+    host: /^imx\.to$/,
   },
   async ready() {
     let a = $.$("#continuebutton, .button");
     if (a) {
       a.click();
-    } else {
-      a = $.$("#imgContinue, .button");
-      if (a) {
-        a.click();
-      }
     }
     a = $(".centred");
     await $.openImage(a.src);
