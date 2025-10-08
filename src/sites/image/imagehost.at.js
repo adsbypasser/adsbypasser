435c7f68c3fcf4223630cbbb7cb9ae79c481c8a3
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2025-10-08 10:24:49 -0400

    Update trafficimage.club.js
    Moved im.ge

--- src/sites/image/imagehost.at.js
+++ src/sites/image/imagehost.at.js
@@ -1,8 +1,18 @@
 /**
  * @domain imagehost.at
+ * @domain im.ge
  */
-_.register({
-  rule: "https://www.imagehost.at/image/*",
+  _.register({
+    rule: [
+    {
+      host: /^www\.imagehost\.at$/,
+      path: /^\/image\//,
+    },
+    {
+      host: /^im\.ge$/,
+      path: /^\/i\//,
+    }, 
+  ],    
   async ready() {
     const i = $('meta[property="og:image"]');
     await $.openImage(i.content);
--- src/sites/image/trafficimage.club.js
+++ src/sites/image/trafficimage.club.js
@@ -1,19 +1,12 @@
 /**
- * @domain im.ge
  * @domain img.trafficimage.club
  * @domain trafficimage.club
  */
 _.register({
-  rule: [
-    {
-      host: [/^img\.trafficimage\.club$/, /^trafficimage\.club$/],
-      path: /^\/image\//,
-    },
-    {
-      host: /^im\.ge$/,
-      path: /^\/i\//,
-    },
-  ],
+  rule: {
+    host: /^(img\.)?trafficimage\.club$/,
+    path: /^\/image\//,
+  },
   async ready() {
     const img = $('meta[property="og:image"]');
     await $.openImage(img.content);
