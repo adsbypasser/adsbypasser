6a5693bef526702bbfbf71023bc8493fb4b83fca
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-01-01 23:26:57 +0900

    chore: npm run format

--- src/sites/image/fastpic.org.js
+++ src/sites/image/fastpic.org.js
@@ -4,15 +4,15 @@
 _.register({
   rule: {
     host: /^fastpic\.org$/,
-    path: /^\/view\//
+    path: /^\/view\//,
   },
   async ready() {
-  const a = $.$("#imglink");
-  if (a) {
-    await $.openLink(a.href);
-    return;
-  }
-  const directUrl = $.searchFromScripts(/loading_img = '([^"]+)';/);
-  await $.openLink(directUrl[1]);
+    const a = $.$("#imglink");
+    if (a) {
+      await $.openLink(a.href);
+      return;
+    }
+    const directUrl = $.searchFromScripts(/loading_img = '([^"]+)';/);
+    await $.openLink(directUrl[1]);
   },
 });
