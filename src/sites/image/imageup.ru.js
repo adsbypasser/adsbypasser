d453c3e24dd45ca45547563af9714d120e03ed7e
Nemo <callmenemo491@users.noreply.github.com>
2026-02-09 17:58:45 +0900

    retire: imageupper.com

--- src/sites/image/imageup.ru.js
+++ src/sites/image/imageup.ru.js
@@ -1,11 +1,10 @@
 /**
  * @domain imageup.ru
- * @domain imageupper.com
  * @domain imgbox.com
  */
 _.register({
   rule: {
-    host: [/^imageup\.ru$/, /^imageupper\.com$/, /^imgbox\.com$/],
+    host: [/^imageup\.ru$/, /^imgbox\.com$/],
   },
   async ready() {
     const i = $("#img, #image");
