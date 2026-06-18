89757ba78c9f9c7d059953bba25914d9fe874888
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2026-06-18 14:57:04 -0400

    retire: imagehost.at

--- src/sites/image/im.ge.js
+++ src/sites/image/im.ge.js
@@ -1,18 +1,12 @@
 /**
- * @domain imagehost.at
  * @domain im.ge
  */
 _.register({
-  rule: [
-    {
-      host: /^www\.imagehost\.at$/,
-      path: /^\/image\//,
-    },
+  rule:
     {
       host: /^im\.ge$/,
       path: /^\/i\//,
     },
-  ],
   async ready() {
     const i = $('meta[property="og:image"]');
     await $.openImage(i.content);
