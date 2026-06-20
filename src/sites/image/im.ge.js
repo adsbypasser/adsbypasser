6025db681099ce3c697258ea6cc9068ad9729776
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-06-20 23:12:15 +0900

    chore: npm run format

--- src/sites/image/im.ge.js
+++ src/sites/image/im.ge.js
@@ -2,11 +2,10 @@
  * @domain im.ge
  */
 _.register({
-  rule:
-    {
-      host: /^im\.ge$/,
-      path: /^\/i\//,
-    },
+  rule: {
+    host: /^im\.ge$/,
+    path: /^\/i\//,
+  },
   async ready() {
     const i = $('meta[property="og:image"]');
     await $.openImage(i.content);
