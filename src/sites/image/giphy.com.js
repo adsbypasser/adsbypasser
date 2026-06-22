824e0de764be22305cb71770a940cfd6f0396b5f
Reggie521 <35973596+Reggie521@users.noreply.github.com>
2026-06-22 13:09:17 -0400

    feat: giphy.com

---
+++ src/sites/image/giphy.com.js
@@ -0,0 +1,21 @@
+/**
+ * @domain giphy.com
+ */
+_.register({
+  rule: {
+    host: /^giphy\.com$/,
+  },
+  async ready() {
+    const i = $('meta[property="og:image"]');
+    await $.openLink(i.content);
+  },
+});    
+_.register({
+  rule: {
+    host: /^media[0-9]\.giphy\.com$/,
+  },
+  async ready() {
+    const img = $('a img.media_gif__MBeQG');
+    await $.openImage(img.src);
+  },
+});
