0c4b37d281c4e692bfca3200f9439ad5e0f44bce
Nemo <callmenemo491@users.noreply.github.com>
2026-08-01 13:38:23 +0900

    Update domain rules for 3xplanet site
    added:
    * 3xpla.net
    * 3xplanet.xyz
    * javxspot.com

--- src/sites/image/3xplanet.com.js
+++ src/sites/image/3xplanet.com.js
@@ -1,16 +1,21 @@
 /**
+ * @domain 3xpla.net
  * @domain 3xplanet.com
  * @domain 3xplanet.net
+ * @domain 3xplanet.xyz
  * @domain jav-load.com
  * @domain javtenshi.com
+ * @domain javxspot.com
  * @domain uncenav.com
  */
 _.register({
   rule: {
     host: [
-      /^3xplanet\.(com|net)$/,
+      /^3xpla\.net/,
+      /^3xplanet\.(com|net|xyz)$/,
       /^jav-load\.com$/,
       /^javtenshi\.com$/,
+      /^javxspot\.com$/,
       /^uncenav\.com$/,
     ],
     path: /^\/viewimage\//,
