242a85294446a87c4a04b9797b679dd32ba03968
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-09-26 21:34:38 +0900

    chore: npm run format

--- src/sites/image/fastpic.org.js
+++ src/sites/image/fastpic.org.js
@@ -8,44 +8,48 @@ _.register({
   },
   async ready() {
     const findUrl = () => {
-        const links = [document.querySelectorAll("a[href]")];
-        const body = document.body?.textContent?.toLowerCase() || "";
-        const fallback = links.find(a =>
-            /open the image page with this link/i.test(a.textContent)
-        );
+      const links = [document.querySelectorAll("a[href]")];
+      const body = document.body?.textContent?.toLowerCase() || "";
+      const fallback = links.find((a) =>
+        /open the image page with this link/i.test(a.textContent),
+      );
 
-        if (
-            /button not working?/i.test(body) &&
-            /open the image page with this link/i.test(body)
-        ) {
-            return fallback?.href;
-        }
+      if (
+        /button not working?/i.test(body) &&
+        /open the image page with this link/i.test(body)
+      ) {
+        return fallback?.href;
+      }
 
-        return links.find(a =>
-            /fullview/i.test(a.href) &&
-            /continue to image|click to continue to image|перейти к изображению/i.test(a.textContent))?.href;
+      return links.find(
+        (a) =>
+          /fullview/i.test(a.href) &&
+          /continue to image|click to continue to image|перейти к изображению/i.test(
+            a.textContent,
+          ),
+      )?.href;
     };
 
     let url = findUrl();
 
     if (!url) {
-        const scripts = document.scripts;
+      const scripts = document.scripts;
 
-        for (const script of scripts) {
-            const match = script.textContent.match(/pp0["sr"+"c"]="([^"]+)"/);
+      for (const script of scripts) {
+        const match = script.textContent.match(/pp0["sr"+"c"]="([^"]+)"/);
 
-            if (match) {
-                url = match[1];
-                break;
-            }
+        if (match) {
+          url = match[1];
+          break;
         }
+      }
     }
     if (!url) {
-        url = document.querySelector("#imglink, #imga")?.href;
+      url = document.querySelector("#imglink, #imga")?.href;
     }
 
     if (url) {
-        window.location.href = url;
+      window.location.href = url;
     }
- },
+  },
 });
